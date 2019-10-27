c2bcb0fb756c4915033802be9292684618c87e40
Rsantct <rsantct@gmail.com>
2019-10-27 23:56:22 +0100

    Create zita-n2j_mcast.py

---
+++ pe.audio.sys/share/scripts/zita-n2j_mcast.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+    A LAN multicast audio receiver, from Fons Adriaensen's zita-njbridge.
+    
+    See man zita-njbridge:
+    
+        ... similar  to  having analog audio connections between the 
+        sound cards of the systems using it
+
+    usage:    zita-n2j_mcast.py   start | stop
+"""
+import sys
+import subprocess as sp
+
+def start():
+
+    # Getting the machine's used interface name
+    interface = ''
+    tmp = sp.check_output('ip route'.split()).decode()
+    # Example $ ip route
+    # default via 192.168.1.1 dev eth0 proto dhcp src 192.168.1.36 metric 202
+    for line in tmp.split('\n'):
+        if 'default' in line:
+            interface = line.split()[4]
+    if not interface:
+        print( 'init/zita-n2j_mcast: cannot get your network interface name :-/' )
+        return False
+
+    # Using a no reserved multicast address 224.0.0.151 and port 65151
+    # https://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml#multicast-addresses-1
+    sp.Popen( f'zita-n2j --buff 50 224.0.0.151 65151 {interface}'.split() )
+
+def stop():
+    sp.Popen( 'pkill -KILL zita-n2j'.split() )
+
+if __name__ == '__main__':
+
+    if sys.argv[1:]:
+        option = sys.argv[1]
+        if option == 'start':
+            start()
+        elif option == 'stop':
+            stop()
+        else:
+            print(__doc__)
+    else:
+        print(__doc__)
