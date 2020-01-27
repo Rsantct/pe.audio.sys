164e83bf6570d14dc23b714b042e5e4fac7e5953
Rsantct <rsantct@gmail.com>
2020-01-27 17:28:14 +0100

    Update clientside.js

--- pe.audio.sys/share/www/clientside.js
+++ pe.audio.sys/share/www/clientside.js
@@ -35,7 +35,7 @@
 //  (i) Set URL_PREFIX ='/' if you use the provided peasys_node.js server script,
 //      or set it '/functions.php' if you use Apache+PHP at server side.
 //
-const URL_PREFIX = '/';
+const URL_PREFIX = '/functions.php';
 const AUTO_UPDATE_INTERVAL = 1000;      // Auto-update interval millisec
 // -----------------------------------------------------------------------------
 
