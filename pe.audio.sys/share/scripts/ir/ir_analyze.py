cd58a80e3265b7167aa507060415898fc19306b6
Rsantct <rsantct@gmail.com>
2020-02-08 17:36:50 +0100

    Update ir_analyze.py

--- pe.audio.sys/share/scripts/ir/ir_analyze.py
+++ pe.audio.sys/share/scripts/ir/ir_analyze.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 """ BETA EXPERIMENTAL
 """
+# CHUNK DIVIDER (figure out by analyzing the raw plotting below)
+DIVIDER = b'\x00'
 
 from matplotlib import pyplot as plt
 import sys
 
-# CHUNK DIVIDER (figure out by analyzing the raw plotting below)
-DIVIDER = b'\x00'
+def b2hex(b):
+    """ converts a bytes stream into a easy readable raw hex list, e.g:
+        in:     b'\x00-m-i)m\xbb\xff'
+        out:    ['00', '2d', '6d', '2d', '69', '29', '6d', 'bb', 'ff']
+    """
+    bh = b.hex()
+    return [ bh[i*2:i*2+2] for i in range(int(len(bh)/2)) ]
+
 
 log = open( sys.argv[1], 'rb').read()
-# split into chunks by values zero
+# split into chunks by the divider
 chunks = log.split(DIVIDER)
-# restore the zeros
+# restore the divider
 chunks = [DIVIDER + x for x in chunks]
 
 # Search for uniques and counting their occurrences
@@ -26,18 +34,13 @@ for chunk in chunks:
 
 # Print results
 print()
-print('times:   sequence:')
+print('times:  sequence:')
 for unique in sorted(uniques):
+    # times
     print( str(uniques[unique]).rjust(4) + '    ',  end='')
-    for b in unique:
-        print(str(b).rjust(4), end='')
-    print()
+    #            sequence
+    print( ' '.join( b2hex(unique) ) )
 
-print()
-print('         (as bytes)')
-for unique in sorted(uniques):
-    print( str(uniques[unique]).rjust(4) + '    ',  end='')
-    print(unique)
 
 # Raw plotting
 bytes2ints = [x for x in log]
