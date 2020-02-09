6a5a77d8a6354a5f60416571f10cd50ca727b2c6
Rsantct <rsantct@gmail.com>
2020-02-09 20:55:06 +0100

    Update ir_analyze.py

--- pe.audio.sys/share/scripts/ir/ir_analyze.py
+++ pe.audio.sys/share/scripts/ir/ir_analyze.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """ BETA EXPERIMENTAL
+    usage:
+        ir_analize.py  rawBytesLogFname [-np]
+            -np skips to plot the graph
 """
-# CHUNK DIVIDER (figure out by analyzing the raw plotting below)
+
+# CONFIGURE HERE THE CHUNK DIVIDER
+# (i) usually 00, but figure out by analyzing the ir.py -t console
+#     and the raw plotting below
 DIVIDER = b'\x00'
+
 
 from matplotlib import pyplot as plt
 import sys
@@ -16,13 +23,18 @@ def b2hex(b):
     return [ bh[i*2:i*2+2] for i in range(int(len(bh)/2)) ]
 
 
+if not sys.argv[1:]:
+    print(__doc__)
+    exit()
+
+# read the rawBytesLogFname
 log = open( sys.argv[1], 'rb').read()
 # split into chunks by the divider
 chunks = log.split(DIVIDER)
 # restore the divider
 chunks = [DIVIDER + x for x in chunks]
 
-# Search for uniques and counting their occurrences
+# search for uniques and counting their occurrences
 uniques={}
 for chunk in chunks:
     if not chunk:
@@ -32,7 +44,7 @@ for chunk in chunks:
     else:
         uniques[chunk] = uniques[chunk] + 1
 
-# Print results
+# print results
 print()
 print('times:  sequence:')
 for unique in sorted(uniques):
@@ -42,7 +54,10 @@ for unique in sorted(uniques):
     print( ' '.join( b2hex(unique) ) )
 
 
-# Raw plotting
+if "-np" in sys.argv[1:]:
+    exit()
+
+# raw plotting
 bytes2ints = [x for x in log]
 plt.plot(bytes2ints)
 plt.title(sys.argv[1])
