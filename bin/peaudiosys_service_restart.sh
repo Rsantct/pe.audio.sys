ee096afc630dc21e9977d3582f50d0ea093ac3fc
Rsantct <rsantct@gmail.com>
2020-03-17 13:03:53 +0100

    Update peaudiosys_service_restart.sh

--- bin/peaudiosys_service_restart.sh
+++ bin/peaudiosys_service_restart.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 svc=$1
+opc=$2
 
 if [[ -z $svc ]]; then
-    echo "usage:    peaudiosys_service_restart.sh   <service>  [stop]"
+    echo "usage:    peaudiosys_service_restart.sh   <service>  [stop | --debug]"
+    echo ""
+    echo "          --debug     will keep messages to console,"
+    echo "                      otherways will redirect to /dev/null"
     exit 0
 fi
 
@@ -15,10 +19,9 @@ fi
 # Killing the running service:
 # (triple quoted is needed for this to work)
 pkill -KILL -f """server.py $svc"""
-if [[ $2 = 'stop' ]]; then
+if [[ $opc == 'stop' ]]; then
     exit 0
 fi
-
 sleep .25
 
 # Reading addresses and ports from the pe.audio.sy config file
@@ -32,6 +35,11 @@ if [[ ! $SRV_ADDR ]]; then
 fi
 
 # Launching again the service.
-# (i) It is IMPORTANT to redirect stdout & stderr to keep it 
-# alive even if the launcher session has been closed (e.g. a crontab job)
-python3 ~/pe.audio.sys/share/server.py "$svc" "$SRV_ADDR" "$SRV_PORT" "$2" 1>/dev/null 2>&1 &
+# (i) It is IMPORTANT to redirect stdout & stderr to keep it alive even
+#     if the launcher session has been closed (e.g. a crontab job),
+#     except if -d --debug is indicated
+if [[ $opc == *"-d"* ]]; then
+    python3 ~/pe.audio.sys/share/server.py "$svc" "$SRV_ADDR" "$SRV_PORT" "$2" &
+else
+    python3 ~/pe.audio.sys/share/server.py "$svc" "$SRV_ADDR" "$SRV_PORT" "$2" 1>/dev/null 2>&1 &
+fi
