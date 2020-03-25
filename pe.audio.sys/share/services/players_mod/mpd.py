c6239d575c18abd36921f0ee534b64bf1eb8d66e
Rsantct <rsantct@gmail.com>
2020-03-25 19:42:23 +0100

    Update mpd.py

--- pe.audio.sys/share/services/players_mod/mpd.py
+++ pe.audio.sys/share/services/players_mod/mpd.py
@@ -37,15 +37,25 @@ METATEMPLATE = {
     'artist':       '',
     'album':        '',
     'title':        '',
-    'track_num':    ''
+    'track_num':    '',
+    'state':        'stop'
     }
+
+# Auxiliary function to format hh:mm:ss
+def timeFmt(x):
+    # x must be float
+    h = int( x / 3600 )         # hours
+    x = int( round(x % 3600) )  # updating x to reamining seconds
+    m = int( x / 60 )           # minutes from the new x
+    s = int( round(x % 60) )    # and seconds
+    return f'{h:0>2}:{m:0>2}:{s:0>2}'
 
 # MPD control, status and metadata
 def mpd_client(query):
     """ Comuticates to MPD music player daemon
         Input: a command to query to the MPD daemon
-        Returns: the MPD response.
-        I/O: .mpd_events (w)
+        Returns: the MPD response: pb state word or metadata json string.
+        I/O: .mpd_metadata (w)
     """
 
     def get_meta():
@@ -54,82 +64,86 @@ def mpd_client(query):
         md = METATEMPLATE.copy()
         md['player'] = 'MPD'
 
-        if mpd_online:
+        # (i) Not all tracks have complete currentsong() fields:
+        # artist, title, album, track, etc fields may NOT be provided
+        # file, time, duration, pos, id           are ALWAYS provided
 
-            # We try because not all tracks have complete metadata fields:
-            try:    md['artist']    = client.currentsong()['artist']
-            except: pass
-            try:    md['album']     = client.currentsong()['album']
-            except: pass
-            try:    md['title']     = client.currentsong()['title']
-            except: pass
-            try:    md['track_num'] = client.currentsong()['track']
-            except: pass
-            try:    md['bitrate']   = client.status()['bitrate']   # given in kbps
-            except: pass
-            try:    md['time_pos']  = timeFmt( float( client.status()['elapsed'] ) )
-            except: pass
-            try:    md['time_tot']  = timeFmt( float( client.currentsong()['time'] ) )
-            except: pass
+        try:    md['title']     = client.currentsong()['title']
+        except: md['title']     = client.currentsong()['file'] \
+                                               .split('/')[-1]
+        try:    md['artist']    = client.currentsong()['artist']
+        except: pass
 
-            client.close()
+        try:    md['album']     = client.currentsong()['album']
+        except: pass
 
-        # As an add-on, we will update an event file on the flavour of mplayer or librespot,
-        # to be monitored by any event changes service as for example lcd_service.py
-        with open( f'{MAINFOLDER}/.mpd_events', 'w' ) as file:
+        try:    md['track_num'] = client.currentsong()['track']
+        except: pass
+
+        try:    md['bitrate']   = client.status()['bitrate'] # kbps
+        except: pass
+
+        try:    md['time_pos']  = timeFmt( float(
+                                    client.status()['elapsed'] ) )
+        except: pass
+
+        try:    md['time_tot']  = timeFmt( float( 
+                                    client.currentsong()['time'] ) )
+        except: pass
+
+        try:    md['state'] = client.status()['state']
+        except: pass
+
+        # As an add-on, we will dump metadata to a file
+        with open( f'{MAINFOLDER}/.mpd_metadata', 'w' ) as file:
             file.write( json.dumps( md ) )
 
         return json.dumps( md )
 
     def state():
-        if mpd_online:
-            return client.status()['state']
+        return client.status()['state']
 
     def stop():
-        if mpd_online:
-            client.stop()
-            return client.status()['state']
+        client.stop()
+        return client.status()['state']
 
     def pause():
-        if mpd_online:
-            client.pause()
-            return client.status()['state']
+        client.pause()
+        return client.status()['state']
 
     def play():
-        if mpd_online:
-            client.play()
-            return client.status()['state']
+        client.play()
+        return client.status()['state']
 
     def next():
-        if mpd_online:
-            try:    client.next() # avoids error if some playlist has wrong items
-            except: pass
-            return client.status()['state']
+        try:
+            client.next() # avoids error if some playlist has wrong items
+        except:
+            pass
+        return client.status()['state']
 
     def previous():
-        if mpd_online:
-            try:    client.previous()
-            except: pass
-            return client.status()['state']
+        try:
+            client.previous()
+        except:
+            pass
+        return client.status()['state']
 
-    def rew():                    # for REW and FF will move 30 seconds
-        if mpd_online:
-            client.seekcur('-30')
-            return client.status()['state']
+    def rew(): # for REW and FF will move 30 seconds
+        client.seekcur('-30')
+        return client.status()['state']
 
     def ff():
-        if mpd_online:
-            client.seekcur('+30')
-            return client.status()['state']
+        client.seekcur('+30')
+        return client.status()['state']
 
     client = mpd.MPDClient()
     try:
         client.connect(MPD_HOST, MPD_PORT)
         if MPD_PASSWD:
             client.password(MPD_PASSWD)
-        mpd_online = True
     except:
-        mpd_online = False
+        return ''
 
     result = {  'get_meta':   get_meta,
                 'state':      state,
@@ -142,5 +156,6 @@ def mpd_client(query):
                 'ff':         ff
              }[query]()
 
+    client.close()
     return result
 
