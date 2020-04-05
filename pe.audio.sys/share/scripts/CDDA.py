8ba02765c0a1bb84d7d4956aed1da556fc1eecf2
Rsantct <rsantct@gmail.com>
2020-04-05 13:00:53 +0200

    Update CDDA.py

--- pe.audio.sys/share/scripts/CDDA.py
+++ pe.audio.sys/share/scripts/CDDA.py
@@ -21,31 +21,8 @@
 
     Also used to CD play control and disk eject.
 
-    Usage:      CDDA  start   [track] - Loads Mplayer for CDDA
-                      stop            - KILLS this script
-
-                      control_play [track]
-                      control_pause
-                      control_prev
-                      control_next
-                      control_stop
-                      control_eject
+    Usage:      CDDA  start | stop | eject
 """
-# --- Some info about Mplayer SLAVE commands ---
-#
-# loadfile cdda://A-B:S     play tracks from A to B at speed S
-#
-# get_property filename     get the tracks to be played as
-#                           'A' (single track)
-#                           or 'A-B' (range of tracks)
-#
-# get_property chapter      get the current track index inside
-#                           the filename property (first is 0)
-#
-# seek_chapter 1            go to next track
-# seek_chapter -1           go to prev track
-#
-# seek X seconds
 
 import os
 UHOME = os.path.expanduser("~")
@@ -83,58 +60,7 @@ except:
     CDROM_DEVICE = '/dev/cdrom'
     print(f'(CDDA.py) Using default \'{CDROM_DEVICE}\'')
 
-def control_play(track_num=1):
-
-    try:
-        print( f'(init/CDDA) trying to load track #{track_num}' )
-        # The whole address after 'loadfile' needs to be SINGLE quoted to load properly
-        command = ('loadfile \'cdda://' + str(track_num)  + '-100:1\'\n' )
-        #print(command) # debug
-        f = open( input_fifo, 'w')
-        f.write(command)
-        f.close()
-        return True
-    except:
-        print( f'(init/CDDA) failed to load track \#\'{track_num}\'' )
-        return False
-
-
-def control_seek(step=1):
-    try:
-        command = ('seek_chapter ' + str(step) + '\n' )
-        f = open( input_fifo, 'w')
-        f.write(command)
-        f.close()
-        return True
-    except:
-        print( f'(init/CDDA) failed to change track' )
-        return False
-
-def control_toggle():
-    try:
-        command = ('pause\n' )
-        f = open( input_fifo, 'w')
-        f.write(command)
-        f.close()
-        return True
-    except:
-        print( f'(init/CDDA) failed to toggle pause' )
-        return False
-
-
-def control_stop():
-    try:
-        command = ('stop\n' )
-        f = open( input_fifo, 'w')
-        f.write(command)
-        f.close()
-        return True
-    except:
-        print( f'(init/CDDA) failed to stop playing' )
-        return False
-
-def control_eject():
-    control_stop()
+def eject():
     sp.Popen( f'eject {CDROM_DEVICE}'.split() )
 
 def start():
@@ -149,49 +75,18 @@ def stop():
 
 if __name__ == '__main__':
 
-    ### Reading the command line
     if sys.argv[1:]:
 
         opc = sys.argv[1]
 
-        # STARTS the script and optionally starts playing a track number
         if opc == 'start':
             start()
-            if sys.argv[2:]:
-                opc2 = sys.argv[2]
-                if opc2.isdigit():
-                    control_play( int(opc2) )
-
-        # STOPS (KILLS) all this stuff
         elif opc == 'stop':
             stop()
-
-        # CONTROLLING PLAYING
-        elif opc == 'control_play':
-            if sys.argv[2:]:
-                opc2 = sys.argv[2]
-                if opc2.isdigit():
-                    control_play( int(opc2) )
-            else:
-                control_play()
-
-        elif opc == 'control_toggle' or opc=='control_pause':
-            control_toggle()
-
-        elif opc == 'control_prev':
-            control_seek(-1)
-
-        elif opc == 'control_next':
-            control_seek(1)
-
-        elif opc == 'control_stop':
-            control_stop()
-
-        elif opc == 'control_eject':
-            control_eject()
-
+        elif opc == 'eject':
+            eject()
         else:
-            print( '(init/CDDA) Bad option' )
+            print(__doc__)
 
     else:
         print(__doc__)
