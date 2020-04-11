8159d8684b75789072ce1840585c48953d5ff920
Rsantct <rsantct@gmail.com>
2020-04-11 12:35:32 +0200

    Update peaudiosys.py

--- pe.audio.sys/share/services/peaudiosys.py
+++ pe.audio.sys/share/services/peaudiosys.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with 'pe.audio.sys'.  If not, see <https://www.gnu.org/licenses/>.
 
-""" MAIN MODULE that controls the whole system:
+""" MAIN SERVICE MODULE that controls the whole system:
         preamp, players and auxiliary functions.
     This module is loaded by 'server.py'
 """
