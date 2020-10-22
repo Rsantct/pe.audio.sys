450e1884f28c200c99653ef9029aa716ae162dc0
Rsantct <rsantct@gmail.com>
2020-10-23 01:31:42 +0200

    Create remote_volume.py

---
+++ pe.audio.sys/share/scripts/remote_volume.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+"""
+    Listen for relative volume changes, then forward them
+    to remote listening machines.
+"""
+
+import sys
+import os
+UHOME = os.path.expanduser("~")
+sys.path.append( f'{UHOME}/pe.audio.sys/share' )
+import miscel
+
+from subprocess import Popen
+from time import sleep, time
+import socket
+import json
+# This module is based on monitoring file changes under the pe.audio.sys folder
+#   https://watchdog.readthedocs.io/en/latest/
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+# Handler class to do actions when a file change occurs.
+class files_event_handler(FileSystemEventHandler):
+    """ will do something when <wanted_path> file changes
+    """
+    # (i) This is an inherited class from the imported one 'FileSystemEventHandler',
+    #     which provides the 'event' propiertie.
+    #     Here we expand the class with our custom parameter 'wanted_path'.
+
+    def __init__(self, wanted_path='', antibound=True):
+        self.wanted_path = wanted_path
+        self.antibound = antibound
+        self.ts = time()
+
+    def on_modified(self, event):
+        # DEBUG
+        #print( f'(i) event type: {event.event_type}, file: {event.src_path}' )
+        if event.src_path == self.wanted_path:
+            if self.antibound:
+                if (time() - self.ts) > .1:
+                    main_do()
+                self.ts = time()
+            else:
+                main_do()
+
+
+def read_last_line(fname=''):
+    # source:
+    # https://stackoverflow.com/questions/46258499/read-the-last-line-of-a-file-in-python
+    # For large files it would be more efficient to seek to the end of the file,
+    # and move backwards to find a newline.
+    # Note that the file has to be opened in binary mode, otherwise,
+    # it will be impossible to seek from the end.
+
+    if not fname:
+        return ''
+
+    try:
+        with open(fname, 'rb') as f:
+            f.seek(-2, os.SEEK_END)
+            while f.read(1) != b'\n':
+                f.seek(-2, os.SEEK_CUR)
+            last_line = f.readline().decode()
+        return last_line.strip()
+
+    except:
+        return ''
+
+
+def main_do():
+    """ Read the last command, if it was about relative level change
+        then forward it to remotes listeners.
+    """
+    cmd = read_last_line( peaudiosys_log )
+    cmd = cmd.split(';')[1].strip()
+    if 'level' not in cmd or 'add' not in cmd:
+        return
+    for cli_addr in zitaclients:
+        miscel.send_cmd( cmd, host=cli_addr, verbose=True )
+
+
+def killme():
+    Popen( f'pkill -f "scripts/remote_volume.py start"', shell=True )
+    sys.exit()
+
+
+if __name__ == "__main__":
+
+
+    for opc in sys.argv[1:]:
+        if opc == 'stop':
+            killme()
+        elif opc == 'start':
+            pass
+        else:
+            print(__doc__)
+            sys.exit()
+
+
+    my_hostname     = socket.gethostname()
+    my_ip           = socket.gethostbyname(f'{my_hostname}.local')
+    peaudiosys_log  = f'{UHOME}/pe.audio.sys/.peaudiosys_cmd.log'
+
+    zitaclients=[]
+
+    print('Looking for remote listening machines')
+    nrange=range(233, 240)
+    for n in nrange:
+        addr = f'192.168.1.{str(n)}'
+        if addr == my_ip:
+            continue
+        ans = miscel.send_cmd('state', timeout=.5, host=addr)
+
+        if 'no answer' in ans:
+            continue
+
+        source = ''
+        try:
+            source = json.loads(ans)["input"]
+        except:
+            pass
+        if 'remote' in source:
+            zitaclients.append(addr)
+
+    print(zitaclients)
+
+    # Starts a WATCHDOG to observe file changes
+    #   https://watchdog.readthedocs.io/en/latest/
+    #   https://stackoverflow.com/questions/18599339/
+    #   python-watchdog-monitoring-file-for-changes
+    #   Use recursive=True to observe also subfolders
+    #   Even observing recursively the CPU load is negligible,
+    #   but we prefer to observe to a single folder.
+
+    # Will observe for changes in <.state.yml> under <pe.audio.sys> folder:
+    observer = Observer()
+    observer.schedule( files_event_handler( wanted_path=peaudiosys_log,
+                                            antibound=True ),
+                       path=f'{UHOME}/pe.audio.sys',
+                       recursive=False )
+    observer.start()
+    observer.join()
