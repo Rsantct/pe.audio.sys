a80eec308f4fd319bc2e439dbeb4a02e09ba22be
Rsantct <rsantct@gmail.com>
2020-12-17 19:32:48 +0100

    Update peaudiosys_plot_eq_curves.py

--- bin/peaudiosys_plot_eq_curves.py
+++ bin/peaudiosys_plot_eq_curves.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-    Plots the curves inside a .dat file of those used in
-    the Brutefir EQ stage of FIRtro / pre.di.c / pe.audio.sys
+    Plots the available curves for the Brutefir smooth EQ module,
+    by default located under the 'pe.audio.sys/share/eq' folder
 
     usage:
 
@@ -10,78 +10,64 @@
         -pha    adds the phase plot
 
 """
-
 import sys, os
 import numpy as np
 from matplotlib import pyplot as plt
 
+UHOME       = os.path.expanduser("~")
+EQ_FOLDER   = f'{UHOME}/pe.audio.sys/share/eq'
+
 def get_curve_files(fpattern):
 
-    try:
-        freq_files = [x for x in EQ_FILES if 'freq.dat' in x ]
-        if len(freq_files) > 1:
-            raise
-        else:
-            freq_file = freq_files[0]
-    except:
-        print( f'\n(!) Problems reading a unique \'xxxfreq.dat\' '
-               f'at \'{EQ_FOLDER}\'' )
-        exit()
+    mag_files = [x for x in EQ_FILES if (fpattern in x) and ('mag.dat' in x) ]
 
-    try:
-        mag_files = [x for x in EQ_FILES if f'{fpattern}_mag.dat' in x ]
-        if len(mag_files) > 1:
-            raise
-        else:
-            mag_file = mag_files[0]
-    except:
-        print( f'\n(!) problems reading a unique \'***{fpattern}_mag.dat\' '
-               f'at \'{EQ_FOLDER}\'' )
-        exit()
+    if len(mag_files) == 0:
+        raise ValueError(f'not files found for pattern \'{fpattern}\'')
+    elif len(mag_files) > 1:
+        raise ValueError(f'too much \'{fpattern}\' .dat files')
 
+    mag_file = mag_files[0]
     pha_file = mag_file.replace('_mag', '_pha')
 
-    return freq_file, mag_file, pha_file
+    return mag_file, pha_file
+
+
+def get_freq_file():
+
+    freq_files = [x for x in EQ_FILES if 'freq.dat' in x ]
+
+    if len(freq_files) > 1:
+        raise ValueError('too much freq.dat files')
+
+    return freq_files[0]
 
 
 if __name__ == '__main__':
 
-    HOME = os.path.expanduser("~")
-    EQ_FOLDER = ''
-
-    # Try to read the optionals /path/to/eq_files_folder
-    #                           -pha
     pha = False
-    if sys.argv[2:]:
+    if not sys.argv[1:]:
+        print(__doc__)
+        exit()
+    else:
+        fpattern = sys.argv[1]
         for opc in sys.argv[2:]:
             if '-pha' in opc:
                 pha = True
             elif '-h' in opc[0:]:
                 print(__doc__)
                 exit()
-            else:
+            elif opc[0] != '-':
                 EQ_FOLDER = opc
-                EQ_FILES = os.listdir(EQ_FOLDER)
 
-    if not EQ_FOLDER:
-        EQ_FOLDER = f'{HOME}/pe.audio.sys/share/eq'
-        EQ_FILES = os.listdir(EQ_FOLDER)
+    EQ_FILES = os.listdir(EQ_FOLDER)
 
     # Read the filenames set for the given pattern
-    try:
-        freq_fname, mag_fname, pha_fname = get_curve_files( sys.argv[1] )
-    except:
-        print(__doc__)
-        exit()
+    freq_fname              = get_freq_file()
+    mag_fname, pha_fname    = get_curve_files( fpattern )
 
-    # A .dat file can have one o more curves inside.
     freq = np.loadtxt( f'{EQ_FOLDER}/{freq_fname}' )
-    mags = np.loadtxt( f'{EQ_FOLDER}/{mag_fname}' )
-    phas = np.loadtxt( f'{EQ_FOLDER}/{pha_fname}' )
-
-    if 'target' in mag_fname:
-        mags = mags.transpose()
-        phas = phas.transpose()
+    mags = np.loadtxt( f'{EQ_FOLDER}/{mag_fname}'  )
+    phas = np.loadtxt( f'{EQ_FOLDER}/{pha_fname}'  )
 
     # Prepare the plot
     fig = plt.figure()
@@ -92,31 +78,11 @@ if __name__ == '__main__':
         ax2 = fig.add_subplot(2, 1, 2)
         fig.subplots_adjust(hspace=.4)
 
+    for curve in mags:
+        ax1.semilogx ( freq, curve )
 
-    # A single target curve kind of
-    if len( mags.shape ) == 1:
-        ax1.semilogx ( freq, mags )
-        ax1.set_ylim(-6,12)
-        if pha:
-            ax2.semilogx ( freq, phas )
-
-    # Multiple curves tone or loudness kind of.
-    # Looping over the curves inside the .dat file
-    else:
-        for idx in range( mags.shape[1] ):
-            ax1.semilogx ( freq, mags[:,idx], label=idx)
-        if not pha:
-            ax1.legend( loc="center", bbox_to_anchor=(1.15, 1.05) )
-            handles, labels = ax1.get_legend_handles_labels()
-            ax1.legend(handles[::-1], labels[::-1])
-        if pha:
-            for idx in range( phas.shape[1] ):
-                ax2.semilogx ( freq, phas[:,idx], label=idx)
-
-    ax1.set_title( mag_fname )
     if pha:
-        ax2.set_title( pha_fname )
+        for curve in phas:
+            ax2.semilogx ( freq, curve )
 
     plt.show()
-
-
