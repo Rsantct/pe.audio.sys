3cee66caca3507a29945b6fc799bd00b34e8422e
Rsantct <rsantct@gmail.com>
2020-12-30 19:54:24 +0100

    Update and rename zita-n2j.py to zita_link.py

--- pe.audio.sys/share/scripts/zita_link.py
+++ pe.audio.sys/share/scripts/zita_link.py
@@ -7,32 +7,25 @@
         ... similar  to  having analog audio connections between the
         sound cards of the systems using it
 
-    usage:    zita-n2j.py   start | stop
+    Usage:    zita_link.py   start | stop
 
 
-    (i)
+    NOTICE:
 
     This script replaces the former 'zita-xxx_mcast.py' versions.
 
     It works just from the receiver side. The sender side zita-j2n process
-    will be automagically triggered from here.
+    will be automagically triggered on demand from here.
 
 """
-import sys
-from subprocess import Popen, check_output
+from subprocess import Popen
 from time import sleep
-import yaml
-import ipaddress
+import sys
 import os
-
 UHOME = os.path.expanduser("~")
 sys.path.append(f'{UHOME}/pe.audio.sys/share')
-from miscel import is_IP, get_my_ip
+from miscel import is_IP, get_my_ip, CONFIG, send_cmd
 
-
-# (i)   zita-n2j does consume high CPU% while receiving because internal
-#       resampling is used always.
-#       If not synced with sender, CPU% will be zero.
 
 # Additional buffering (ms) (default 10, safe value 50, WiFi 100)
 BUFFER = 50
@@ -42,7 +35,7 @@ def start():
 
     # RUN REMOTE SENDER:
     remotecmd   = f'aux zita_client {my_ip} start'
-    Popen( f'echo "{remotecmd}" | nc -N {remote_addr} 9990', shell=True)
+    send_cmd(remotecmd, host=remote_addr, port=remote_port)
 
     # RUN LOCAL RECEIVER:
     zitacmd = f'zita-n2j --jname {remote_addr} --buff {BUFFER} {my_ip} {zitaport}'
@@ -51,25 +44,17 @@ def start():
 
 
 def stop():
+
     # REMOTE
-    my_ip       = get_my_ip()
     remotecmd   = f'aux zita_client {my_ip} stop'
-    Popen( f'echo "{remotecmd}" | nc -N {remote_addr} {remote_port}', shell=True)
+    send_cmd(remotecmd, host=remote_addr, port=remote_port)
 
     # LOCAL
     Popen( 'pkill -KILL zita-n2j'.split() )
     sleep(1)
 
 
-if __name__ == '__main__':
-
-    if not sys.argv[1:]:
-        print(__doc__)
-        sys.exit()
-
-    with open(f'{UHOME}/pe.audio.sys/config.yml', 'r') as f:
-        CONFIG = yaml.safe_load(f.read())
-
+def get_remote():
     # Retrieving the remote sender address from 'config.yml'.
     # For a 'remote.....' named source, it is expected to have
     # an IP address kind of in its capture_port field:
@@ -83,6 +68,16 @@ if __name__ == '__main__':
             if is_IP(cport):
                 remote_addr = cport
                 break
+    return source, remote_addr, remote_port
+
+
+if __name__ == '__main__':
+
+    if not sys.argv[1:]:
+        print(__doc__)
+        sys.exit()
+
+    source, remote_addr, remote_port = get_remote()
 
     if not remote_addr:
         print(f'(zita-n2j) Cannot get remote address from configured sources')
