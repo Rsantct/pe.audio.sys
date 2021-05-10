52361238f30cf00e21bb96543d3f5eae08afb260
Rsantct <rsantct@gmail.com>
2021-05-10 18:53:08 +0200

    Update zita_link.py

--- pe.audio.sys/share/scripts/zita_link.py
+++ pe.audio.sys/share/scripts/zita_link.py
@@ -45,7 +45,7 @@ import sys
 import os
 UHOME = os.path.expanduser("~")
 sys.path.append(f'{UHOME}/pe.audio.sys/share')
-from miscel import is_IP, get_my_ip, CONFIG, send_cmd
+from miscel import is_IP, get_my_ip, CONFIG, send_cmd, get_remote_source_info
 
 
 # Additional buffering (ms) (default 10, safe value 50, WiFi 100)
@@ -75,40 +75,13 @@ def stop():
     sleep(1)
 
 
-def get_remote():
-    # Retrieving the remote sender address from 'config.yml'.
-    # For a 'remote.....' named source, it is expected to have
-    # an IP address kind of in its capture_port field:
-    #   capture_port:  X.X.X.X
-    # so this way we can query the remote sender to run 'zita-j2n'
-
-    remote_addr = ''
-    remote_port = 9990
-
-    for source in CONFIG["sources"]:
-        if 'remote' in source:
-            cport = CONFIG["sources"][source]["capture_port"]
-            if is_IP(cport):
-                remote_addr = cport
-                break
-
-    if not remote_addr:
-        print(f'(zita-n2j) Cannot get remote address from configured sources')
-        sys.exit()
-    if not is_IP(remote_addr):
-        print(f'(zita-n2j) source: \'{source}\' address: \'{remote_addr}\' not valid')
-        sys.exit()
-
-    return source, remote_addr, remote_port
-
-
 if __name__ == '__main__':
 
     if not sys.argv[1:]:
         print(__doc__)
         sys.exit()
 
-    source, remote_addr, remote_port = get_remote()
+    source, remote_addr, remote_port = get_remote_source_info()
 
     # The zita UDP port is derived from the last octet of my IP
     my_ip       = get_my_ip()
