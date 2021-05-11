cf41c62740642dff9421a81f539c90189d3933c5
Rsantct <rsantct@gmail.com>
2021-05-11 23:02:03 +0200

    Update start.py

--- pe.audio.sys/start.py
+++ pe.audio.sys/start.py
@@ -39,18 +39,17 @@
 
 """
 
+import subprocess as sp
+from   time import sleep, time, ctime
+import yaml
 import os
 import sys
 UHOME = os.path.expanduser("~")
 sys.path.append(f'{UHOME}/pe.audio.sys')
+from   share.miscel import *
 
-from    share.miscel import *
-import  subprocess as sp
-from    time import sleep, time, ctime
-import  yaml
-import  jack
 
-ME              = __file__.split('/')[-1]
+ME = __file__.split('/')[-1]
 
 
 def prepare_extra_cards(channels=2):
@@ -107,21 +106,29 @@ def check_jloops():
         return True
 
     # Waiting for all loops to be spawned
-    jcli = jack.Client(name='loop_check', no_start_server=True)
     tries = 10
     while tries:
         # The running ones
-        run_loops = [p.name for p in jcli.get_ports('loop')]
+        run_loops = sp.check_output(['jack_lsp', 'loop']).decode().split()
         if sorted(cfg_loops) == sorted(run_loops):
             break
         sleep(1)
         tries -= 1
-    jcli.close()
     if tries:
         print(f'{Fmt.BLUE}({ME}) JACK LOOPS STARTED{Fmt.END}')
         return True
     else:
         print(f'{Fmt.BOLD}({ME}) JACK LOOPS FAILED{Fmt.END}')
+        return False
+
+
+def jack_is_running():
+    ''' checks for jackd process to be running
+    '''
+    try:
+        sp.check_output('jack_lsp >/dev/null 2>&1'.split())
+        return True
+    except sp.CalledProcessError:
         return False
 
 
@@ -372,8 +379,8 @@ if __name__ == "__main__":
         print(f'{Fmt.MAGENTA}({ME}) Managing a temporary \'core\' instance.{Fmt.END}')
 
         # - BRUTEFIR
-        bfstart = core.restart_and_reconnect_brutefir( ['pre_in_loop:output_1',
-                                                        'pre_in_loop:output_2'] )
+        bfstart = core.bf_restart_and_reconnect( ['pre_in_loop:output_1',
+                                                  'pre_in_loop:output_2'] )
         if bfstart == 'done':
             print(f'{Fmt.BOLD}{Fmt.BLUE}({ME}) BRUTEFIR STARTED.{Fmt.END}')
         else:
