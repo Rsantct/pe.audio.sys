733dbf570881d42780e83fbaf28aac1bd9f22392
Rsantct <rsantct@gmail.com>
2021-05-11 23:25:25 +0200

    Create jack_mod.py

---
+++ pe.audio.sys/share/services/preamp_mod/jack_mod.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 Rafael Sánchez
+# This file is part of 'pe.audio.sys', a PC based personal audio system.
+#
+# This is based on 'pre.di.c,' a preamp and digital crossover
+# https://github.com/rripio/pre.di.c
+# Copyright (C) 2018 Roberto Ripio
+# 'pre.di.c' is based on 'FIRtro', a preamp and digital crossover
+# https://github.com/AudioHumLab/FIRtro
+# Copyright (c) 2006-2011 Roberto Ripio
+# Copyright (c) 2011-2016 Alberto Miguélez
+# Copyright (c) 2016-2018 Rafael Sánchez
+#
+# 'pe.audio.sys' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'pe.audio.sys' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'pe.audio.sys'.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import threading
+import jack
+
+
+JCLI = jack.Client('pe.audio.sys', no_start_server=True)
+
+
+def jack_connect(p1, p2, mode='connect', wait=1):
+    """ Low level tool to connect / disconnect a pair of ports,
+        by retriyng for a while
+    """
+    # Will retry during <wait> seconds, this is useful when a
+    # jack port exists but it is still not active,
+    # for instance Brutefir ports takes some seconds to be active.
+
+    wait = int(wait)
+    # will retry every second
+    while wait > 0 :
+        try:
+            if 'dis' in mode or 'off' in mode:
+                JCLI.disconnect(p1, p2)
+            else:
+                JCLI.connect(p1, p2)
+            return 'done'
+        except jack.JackError as e:
+            print( f'(core.jack_connect) Exception: {e}' )
+            return e
+        wait -= 1
+        sleep(1)
+
+
+def jack_connect_bypattern( cap_pattern, pbk_pattern, mode='connect', wait=1 ):
+    """ High level tool to connect/disconnect a given port name patterns
+    """
+    # Try to get ports by a port name pattern
+    cap_ports = JCLI.get_ports( cap_pattern, is_output=True )
+    pbk_ports = JCLI.get_ports( pbk_pattern, is_input=True )
+
+    # If not found, it can be an ALIAS pattern
+    # (if used, jackd -Ln loopback ports will be aliased with the source name)
+    if not cap_ports:
+        loopback_cap_ports = JCLI.get_ports( 'loopback', is_output=True )
+        for p in loopback_cap_ports:
+            # A port can have 2 alias, our is the 2nd one.
+            if cap_pattern in p.aliases[1]:
+                cap_ports.append(p)
+    if not pbk_ports:
+        loopback_pbk_ports = JCLI.get_ports( 'loopback', is_input=True )
+        for p in loopback_pbk_ports:
+            if pbk_pattern in p.aliases[1]:
+                pbk_ports.append(p)
+
+    #print('CAPTURE  ====> ', cap_ports)  # DEBUG
+    #print('PLAYBACK ====> ', pbk_ports)
+    errors = ''
+    if not cap_ports:
+        tmp = f'cannot find jack port "{cap_pattern}" '
+        print(f'(core) {tmp}')
+        errors += tmp
+    if not pbk_ports:
+        tmp = f'cannot find jack port "{pbk_pattern}" '
+        print(f'(core) {tmp}')
+        errors += tmp
+    if errors:
+        return errors
+
+    mode = 'disconnect' if ('dis' in mode or 'off' in mode) else 'connect'
+    for cap_port, pbk_port in zip(cap_ports, pbk_ports):
+        job_jc = threading.Thread( target=jack_connect,
+                                   args=(cap_port, pbk_port, mode, wait) )
+        job_jc.start()
+    return 'ordered'
+
+
+def jack_clear_preamp():
+    """ Force clearing ANY clients, no matter what input was selected
+    """
+    preamp_ports = JCLI.get_ports('pre_in_loop', is_input=True)
+    for preamp_port in preamp_ports:
+        for client in JCLI.get_all_connections(preamp_port):
+            jack_connect( client, preamp_port, mode='off' )
+
+
+def jack_get_samplerate():
+    return
