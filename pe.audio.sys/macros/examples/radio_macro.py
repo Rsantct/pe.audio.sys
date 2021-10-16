f4cbb900a340a1a9f56cb88c878b226b459a129a
Rsantct <rsantct@gmail.com>
2021-10-16 09:13:36 +0200

    Update radio_macro.py

--- pe.audio.sys/macros/examples/radio_macro.py
+++ pe.audio.sys/macros/examples/radio_macro.py
@@ -48,7 +48,8 @@ def main():
     sleep(2)
 
     # Waiting for mplayer ports to re-emerge
-    if not wait4ports( f'mplayer_{mplayer_profile}', timeout=30 ):
+    # (i) Some radio streams take a while to be loaded by Mplayer
+    if not wait4ports( f'mplayer_{mplayer_profile}', timeout=45 ):
         print(f'{Fmt.RED}(macros) ERROR jack ports \'mplayer_{mplayer_profile}\' not found, '
               f'bye :-/{Fmt.RED}')
         sys.exit(-1)
