d84aa44daf9c93e6e4e9f9dd897bd6382fbc4375
Rsantct <rsantct@gmail.com>
2021-12-20 22:46:23 +0100

    Update peaudiosys.py

--- pe.audio.sys/share/services/peaudiosys.py
+++ pe.audio.sys/share/services/peaudiosys.py
@@ -36,13 +36,10 @@ import  threading
 UHOME = os.path.expanduser("~")
 sys.path.append(f'{UHOME}/pe.audio.sys')
 
-from    share.brutefir_mod  import  bf_add_delay, bf_is_running
+from    share.miscel        import  LOG_FOLDER, Fmt
 from    share.services      import  preamp
 from    share.services      import  players
-from    share.miscel        import  *
-
-
-ME                  = __file__.split('/')[-1]
+from    share.services      import  aux
 
 
 # COMMAND LOG FILE
@@ -51,455 +48,8 @@ if os.path.exists(logFname) and os.path.getsize(logFname) > 10e6:
     print ( f"{Fmt.RED}(peaudiosys) log file exceeds ~ 10 MB '{logFname}'{Fmt.END}" )
 print ( f"{Fmt.BLUE}(peaudiosys) logging commands in '{logFname}'{Fmt.END}" )
 
-# Sends audio to a zita-njbridge client
-def send_zita(argvs):
 
-    addr = ''
-    stop = False
-
-    # Reading arguments
-    for argv in argvs.split(' '):
-        if is_IP(argv):
-            addr = argv
-        elif argv == 'stop':
-            stop = True
-
-    # Bad address
-    if not addr:
-        return 'bad address'
-
-    jcli        = jack.Client(name='zitatmp', no_start_server=True)
-    udpport     = addr.split('.')[-1]
-    zitajname   = f'zita-{udpport}'
-
-    # if stop
-    if stop:
-        Popen( f'pkill -KILL -f {zitajname}'.split() )
-        return f'{zitajname} killed'
-
-    jports = jcli.get_ports()
-    if not [x for x in jports if zitajname in x.name]:
-        zitacmd     = f'zita-j2n --jname {zitajname} {addr} 65{udpport}'
-        Popen( zitacmd.split() )
-        sleep(1)
-
-    try:
-        jcli.connect( 'pre_in_loop:output_1', f'zita-{udpport}:in_1' )
-        jcli.connect( 'pre_in_loop:output_2', f'zita-{udpport}:in_2' )
-    except:
-        pass
-
-    jcli.close()
-
-    return 'done'
-
-
-# Read the amplifier state file, if it exists:
-def get_amp_state():
-    curr_sta = 'off'
-    try:
-        with open( f'{AMP_STATE_PATH}', 'r') as f:
-            curr_sta =  f.read().strip()
-    except:
-        pass
-    if not curr_sta or curr_sta.lower() in ('0', 'off'):
-        curr_sta = 'off'
-    elif curr_sta.lower() in ('1', 'on'):
-        curr_sta = 'on'
-    return curr_sta
-
-
-# Set the amplifier switch:
-def set_amp_state(mode):
-    if not AMP_MANAGER:
-        return
-    print( f'({ME}) running \'{AMP_MANAGER.split("/")[-1]} {mode}\'' )
-    Popen( f'{AMP_MANAGER} {mode}', shell=True )
-
-
-def amp_player_manager(mode):
-    """ An auxiliary function for controlling playback
-        from the amplifier switch.
-    """
-
-    if mode == 'off':
-
-        # Do stop playback when switching off the amplifier
-        players.do('stop')
-
-        # Special case: librespot doesn't have playback control feature
-        if 'librespot.py' in CONFIG['scripts']:
-            print(f'{Fmt.BLUE}({ME}) AMP OFF: shutting down librespot process{Fmt.END}')
-            Popen( f'{MAINFOLDER}/share/scripts/librespot.py stop', shell=True)
-            sleep(.5)
-
-    elif mode == 'on':
-        # Do not resume playback when switching on the amplifier
-
-        # Special case: librespot doesn't have playback control feature
-        if 'librespot.py' in CONFIG['scripts']:
-            print(f'{Fmt.BLUE}({ME}) AMP ON: restarting librespot process{Fmt.END}')
-            Popen( f'{MAINFOLDER}/share/scripts/librespot.py start', shell=True)
-            sleep(.5)
-
-
-# LIST OF MACROS under macros/ folder
-def get_macros(only_web_macros=False):
-    """ Return the list of executable files under macros folder. The list
-        can be restricted to web macros NN_xxxxxx, then numeric sorted.
-    """
-    macro_files = []
-
-    with os.scandir( f'{MACROS_FOLDER}' ) as entries:
-
-        for entrie in entries:
-            fname = entrie.name
-
-            # Only executables files
-            if os.path.isfile(f'{MACROS_FOLDER}/{fname}') and \
-               os.access(f'{MACROS_FOLDER}/{fname}', os.X_OK):
-
-                # Web macros are the ones named NN_xxxxxx
-                if only_web_macros:
-                    if fname.split('_')[0].isdigit():
-                        macro_files.append(fname)
-                else:
-                    macro_files.append(fname)
-
-    macro_files.sort()
-
-    # (i) The web page needs a sorted list (numeric sorting only if NN_xxxxxx items)
-    if only_web_macros:
-        macro_files.sort( key=lambda x: int(x.split('_')[0]) )
-
-    return macro_files
-
-
-# Main function for PREAMP/CONVOLVER commands processing
-def process_preamp( cmd, arg='' ):
-    if arg:
-        cmd  = ' '.join( (cmd, arg) )
-    return preamp.do(cmd)
-
-
-# Main function for PLAYERS commands processing
-def process_players( cmd, arg='' ):
-    if arg:
-        cmd  = ' '.join( (cmd, arg) )
-    return players.do(cmd)
-
-
-# Main function for AUX commands processing
-def process_aux( cmd, arg='' ):
-    """ input:  command [, arg]
-        output: an execution result string
-    """
-
-    # On line command list helper
-    def aux_cmd_list():
-        cmd_list = 'amp_switch, get_macros, get_last_macro, run_macro, play, ' + \
-                   'loudness_monitor_reset, lu_monitor_reset, ' + \
-                   'set_loudness_monitor_scope, set_lu_monitor_scope, ' + \
-                   'get_loudness_monitor, get_lu_monitor, ' + \
-                   'restart, get_aux_info, info, add_delay, warning'
-        return cmd_list
-
-    # Aux for playing an url stream
-    def play_istream(url):
-
-        error = False
-
-        # Tune the radio station (Mplayer jack ports will dissapear for a while)
-        Popen( f'{UHOME}/pe.audio.sys/share/scripts/istreams.py url {url}'
-                .split() )
-        # Waits a bit to Mplayer ports to dissapear from jack while loading a new stream.
-        sleep(2)
-        # Waiting for mplayer ports to re-emerge
-        if not wait4ports( f'mplayer_istreams' ):
-            print(f'(peaudiosys.py) ERROR jack ports \'mplayer_istreams\''
-                   ' not found' )
-            error = True
-        if not error:
-            # Switching the preamp input
-            preamp.do('input istreams')
-            return True
-        else:
-            return False
-
-
-    # As per LDCTRL_PATH is a namedpipe (FIFO), it is needed that
-    # 'loudness_monitor.py' was alive in order to release any write to it.
-    # If not alive, any f.write() to LDCTRL_PATH will HANG UP
-    # :-(
-    def lu_ctrl_write(string):
-        try:
-            sp.check_output('pgrep -fla loudness_monitor.py'.split())
-        except:
-            return 'ERROR loudness_monitor.py NOT running'
-        try:
-            with open(LDCTRL_PATH, 'w') as f:
-                f.write(string)
-            return 'ordered'
-        except:
-            return 'unknown ERROR writing .loudness_control FIFO'
-
-
-    # BEGIN of process_aux
-    cmd = cmd.lower()
-
-    # AMPLIFIER SWITCHING
-    if cmd == 'amp_switch':
-
-        # current switch state
-        if arg == 'state':
-            return get_amp_state()
-
-        if arg == 'toggle':
-            # if unknown state, this switch defaults to 'on'
-            result = {'on': 'off', 'off': 'on'}.get( get_amp_state(), 'on' )
-
-        elif arg in ('on', 'off'):
-            result = arg
-
-        set_amp_state( result )
-
-        # optionally will stop the current player
-        if 'amp_off_stops_player' in CONFIG and \
-           CONFIG['amp_off_stops_player'] == True:
-            amp_player_manager(mode=result)
-
-        return result
-
-    # LIST OF MACROS
-    elif cmd == 'get_macros':
-        result = get_macros()
-
-    # LAST EXECUTED MACRO
-    elif cmd == 'get_last_macro':
-        result = AUX_INFO['last_macro']
-
-    # RUN MACRO
-    elif cmd == 'run_macro':
-        if arg in get_macros():
-            print( f'({ME}) running macro: {arg}' )
-            Popen( f'"{MACROS_FOLDER}/{arg}"', shell=True)
-            AUX_INFO["last_macro"] = arg
-            # This updates disk file .aux_info for others to have fresh 'last_macro'
-            dump_aux_info()
-            result = 'ordered'
-        else:
-            result = 'macro not found'
-
-    # PLAYS SOMETHING
-    elif cmd == 'play':
-
-        # An URL: will be played back by the istreams Mplayer service:
-        if arg.startswith('http://') or arg.startswith('https://'):
-            if play_istream(arg):
-                result = 'done'
-            else:
-                result = 'failed'
-        else:
-            result = f'bad: {arg}'
-
-    # RESET the LOUDNESS MONITOR DAEMON:
-    elif cmd == 'loudness_monitor_reset' or \
-         cmd == 'lu_monitor_reset':
-        return lu_ctrl_write('reset')
-
-    # Set the LOUDNESS MONITOR SCOPE:
-    elif cmd == 'set_loudness_monitor_scope' or \
-         cmd == 'set_lu_monitor_scope':
-        return lu_ctrl_write(f'scope={arg}')
-
-    # Get the LOUDNESS MONITOR VALUE from the
-    # loudness monitor daemon's output file:
-    elif cmd == 'get_loudness_monitor' or \
-         cmd == 'get_lu_monitor':
-        try:
-            with open(LDMON_PATH, 'r') as f:
-                result = json.loads( f.read() )
-        except:
-            if 'LU_reset_scope' in CONFIG:
-                result = {'LU_I': 0.0, 'LU_M':0.0,
-                          'scope': CONFIG["LU_reset_scope"]}
-            else:
-                result = {'LU_I': 0.0, 'LU_M':0.0, 'scope': 'album'}
-
-    # Get the AUX_INFO dict
-    elif cmd == 'get_aux_info' or cmd == 'info':
-        return AUX_INFO
-
-    # Add outputs delay, can be useful for multiroom listening
-    elif cmd == 'add_delay':
-        print(f'({ME}) ordering adding {arg} ms of delay.')
-        # (i) Brutefir must be running
-        if not bf_is_running():
-            process_preamp('convolver', 'on')
-        result = bf_add_delay(float(arg))
-
-    # Send zita-j2n to a client (multiroom)
-    elif cmd == 'zita_client':
-        result = send_zita(arg)
-
-    # RESTART
-    elif cmd == 'restart':
-        try:
-            restart_cmd = CONFIG["restart_cmd"]
-        except:
-            restart_cmd = f'{UHOME}/start.py all'
-
-        try:
-            Popen( f'{restart_cmd}'.split() )
-            result = f'ordered: \'{restart_cmd}\''
-        except:
-            print( f'({ME}) error running \'{restart_cmd}\'' )
-            result = f'error running \'{restart_cmd}\''
-
-    # HELP
-    elif cmd == 'help':
-        result = aux_cmd_list()
-
-    # Temporary warning messages (by default expires in 60 s)
-    elif cmd == 'warning':
-
-        args = arg.split()
-
-        if args[0] == 'set':
-
-            if AUX_INFO['warning']:
-                result = 'warning message in use'
-            else:
-                AUX_INFO['warning'] = ' '.join(args[1:])
-                dump_aux_info()
-                warning_expire(timeout=60)
-                result = 'done'
-
-        elif args[0] == 'clear':
-            AUX_INFO['warning'] = ''
-            dump_aux_info()
-            result = 'done'
-
-        elif args[0] == 'get':
-            result = AUX_INFO['warning']
-
-        elif args[0] == 'expire':
-            if args[1:] and args[1].isdigit():
-                warning_expire(timeout=int(args[1]))
-                result = 'done'
-            else:
-                result = 'bad expire timeout'
-        else:
-            result = 'usage: warning set message | warning clear'
-
-    else:
-        result = f'(aux) bad command \'{cmd}\''
-
-    return result
-
-
-# A timer to clear the warning message field inside .aux_info
-def warning_expire(timeout=5):
-    def mytimer(timeout):
-        sleep(timeout)
-        AUX_INFO['warning'] = ''
-        dump_aux_info()
-    job = threading.Thread(target=mytimer, args=(timeout,))
-    job.start()
-
-
-# Dumps pe.audio.sys/.aux_info
-def dump_aux_info():
-    # Dynamic updates
-    AUX_INFO['amp'] =               process_aux('amp_switch', 'state')
-    AUX_INFO['loudness_monitor'] =  process_aux('get_loudness_monitor')
-    # Dumping to disk
-    with open(f'{MAINFOLDER}/.aux_info', 'w') as f:
-        f.write( json.dumps(AUX_INFO) )
-
-
-# Handler class to do actions when a file change occurs.
-class files_event_handler(FileSystemEventHandler):
-    """ will do something when <wanted_path> file changes
-    """
-    # (i) This is an inherited class from the imported one 'FileSystemEventHandler',
-    #     which provides the 'event' propiertie.
-    #     Here we expand the class with our custom parameter 'wanted_path'.
-
-    def __init__(self, wanted_path=''):
-        self.wanted_path = wanted_path
-
-    def on_modified(self, event):
-        # DEBUG
-        #print( f'({ME}) event type: {event.event_type}, file: {event.src_path}' )
-        if event.src_path == self.wanted_path:
-            dump_aux_info()
-
-
-# auto-started when loading this module
-def init():
-
-    global AUX_INFO
-
-
-    # Static web configuration options:
-    def get_web_config():
-
-        wconfig = CONFIG['web_config']
-
-        # Macros used by the web page
-        wconfig['user_macros'] = get_macros(only_web_macros=True)
-
-        # Main selector manages inputs or macros
-        if not 'main_selector' in wconfig:
-            wconfig['main_selector'] = 'inputs';
-        else:
-            if wconfig['main_selector'] not in ('inputs', 'macros'):
-                wconfig['main_selector'] = 'inputs'
-
-        # Complete some additional info
-        wconfig['restart_cmd_info']   = CONFIG['restart_cmd']
-        wconfig['LU_monitor_enabled'] = True if 'loudness_monitor.py' \
-                                                  in CONFIG['scripts'] else False
-
-        return wconfig
-
-
-    AUX_INFO = {    'amp':                  'off',
-                    'loudness_monitor':     0.0,
-                    'web_config':           get_web_config(),
-                    'last_macro':           '-',                # cannot be empty
-                    'warning':              ''
-                }
-
-
-    # First update
-    dump_aux_info()
-
-    # Starts a WATCHDOG to observe file changes
-    #   https://watchdog.readthedocs.io/en/latest/
-    #   https://stackoverflow.com/questions/18599339/
-    #   python-watchdog-monitoring-file-for-changes
-    #   Use recursive=True to observe also subfolders
-    #   Even observing recursively the CPU load is negligible,
-    #   but we prefer to observe to a single folder.
-
-    # Will observe for changes in AMP_STATE_PATH:
-    observer1 = Observer()
-    observer1.schedule( files_event_handler(AMP_STATE_PATH),
-                        path=os.path.dirname(AMP_STATE_PATH),
-                        recursive=False )
-    observer1.start()
-
-    # Will observe for changes in LDMON_PATH:
-    observer2 = Observer()
-    observer1.schedule( files_event_handler(LDMON_PATH),
-                        path=os.path.dirname(LDMON_PATH),
-                        recursive=False )
-    observer2.start()
-
-
-# Interface function to plug this on server.py
+# Interface function for this module
 def do( cmd_phrase ):
 
     def read_cmd_phrase(cmd_phrase):
@@ -510,7 +60,7 @@ def do( cmd_phrase ):
         #           aux     command  arg1 ...
         #     The 'preamp' prefix can be omited
 
-        pfx, cmd, arg = '', '', ''
+        pfx, cmd, argstring = '', '', ''
 
         # This is to avoid empty values when there are more
         # than on space as delimiter inside the cmd_phrase:
@@ -523,33 +73,32 @@ def do( cmd_phrase ):
 
         if chunks[1:]:
             cmd = chunks[1]
-        else:
-            raise Exception(f'({ME}) BAD command: {cmd_phrase}')
         if chunks[2:]:
-            # allows spaces inside the arg part, e.g. 'run_macro 2_Radio Clasica'
-            arg = ' '.join( chunks[2:] )
+            # <argstring> can be compound
+            argstring = ' '.join( chunks[2:] )
 
-        return pfx, cmd, arg
+        return pfx, cmd, argstring
 
 
-    result = 'nothing done'
+    result = f'(peaudiosys) nothing done'
     cmd_phrase = cmd_phrase.strip()
 
     if cmd_phrase:
 
-        pfx, cmd, arg = read_cmd_phrase( cmd_phrase )
-        #print('pfx:', pfx, '| cmd:', cmd, '| arg:', arg) # DEBUG
+        pfx, cmd, args = read_cmd_phrase( cmd_phrase )
+        #print('pfx:', pfx, '| cmd:', cmd, '| args:', args) # DEBUG
 
-        result = {  'preamp':   process_preamp,
-                    'player':   process_players,
-                    'aux':      process_aux }[ pfx ]( cmd, arg )
+        result = {  'preamp':   preamp.do,
+                    'player':   players.do,
+                    'aux':      aux.do
+                  }[ pfx ]( cmd, args )
 
         if type(result) != str:
             result = json.dumps(result)
 
         # Logging avoiding non-relevant commands
         if  ('state'        not in cmd)  and \
-            ('state'        not in arg)  and \
+            ('state'        not in args) and \
             ('get_'         not in cmd)  and \
             ('warning'      not in cmd)  and \
             ('info'         not in cmd):
@@ -561,6 +110,3 @@ def do( cmd_phrase ):
 
     return result
 
-
-# Will AUTO-START init() when loading this module
-init()
