87fe1097cf6d5133a73037463406b74c2af07641
Rsantct <rsantct@gmail.com>
2021-12-26 01:07:24 +0100

    Create config.py

---
+++ pe.audio.sys/share/miscel_mod/config.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2019 Rafael Sánchez
+# This file is part of 'pe.audio.sys', a PC based personal audio system.
+#
+# 'pe.audio.sys' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'pe.audio.sys' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'pe.audio.sys'.  If not, see <https://www.gnu.org/licenses/>.
+
+import  yaml
+from    numpy import loadtxt as np_loadtxt
+import  os
+import  sys
+
+UHOME               = os.path.expanduser("~")
+MAINFOLDER          = f'{UHOME}/pe.audio.sys'
+sys.path.append(f'{UHOME}/pe.audio.sys/share/miscel_mod')
+
+from    fmt import Fmt
+
+CONFIG              = {}
+LOUDSPEAKER         = ''
+EQ_CURVES           = {}
+LSPK_FOLDER         = f'{MAINFOLDER}/loudspeakers/{LOUDSPEAKER}'
+BFCFG_PATH          = f'{LSPK_FOLDER}/brutefir_config'
+
+BFDEF_PATH          = f'{UHOME}/.brutefir_defaults'
+STATE_PATH          = f'{MAINFOLDER}/.state'
+LOG_FOLDER          = f'{MAINFOLDER}/log'
+EQ_FOLDER           = f'{MAINFOLDER}/share/eq'
+MACROS_FOLDER       = f'{MAINFOLDER}/macros'
+LDCTRL_PATH         = f'{MAINFOLDER}/.loudness_control'
+LDMON_PATH          = f'{MAINFOLDER}/.loudness_monitor'
+PLAYER_META_PATH    = f'{MAINFOLDER}/.player_metadata'
+CDDA_INFO_PATH      = f'{MAINFOLDER}/.cdda_info'
+AMP_STATE_PATH      = f'{UHOME}/.amplifier'
+
+
+def _init():
+    """ Autoexec on loading this module
+    """
+    global CONFIG, LOUDSPEAKER, EQ_CURVES, LSPK_FOLDER, BFCFG_PATH
+
+
+    def find_eq_curves():
+        """ Scans share/eq/ and try to collect the whole set of EQ curves
+            needed for the EQ stage in Brutefir (tone and loudness countour)
+            (void)
+        """
+        eq_files = os.listdir(EQ_FOLDER)
+
+        # file names ( 2x loud + 4x tones + freq = total 7 curves)
+        fnames = (  'loudness_mag.dat', 'bass_mag.dat', 'treble_mag.dat',
+                    'loudness_pha.dat', 'bass_pha.dat', 'treble_pha.dat',
+                    'freq.dat' )
+
+        # map dict to get the curve name from the file name
+        cnames = {  'loudness_mag.dat'  : 'loud_mag',
+                    'bass_mag.dat'      : 'bass_mag',
+                    'treble_mag.dat'    : 'treb_mag',
+                    'loudness_pha.dat'  : 'loud_pha',
+                    'bass_pha.dat'      : 'bass_pha',
+                    'treble_pha.dat'    : 'treb_pha',
+                    'freq.dat'          : 'freqs'     }
+
+        pendings = len(fnames)  # 7 curves
+        for fname in fnames:
+
+            # Only one file named as <fname> must be found
+
+            if 'loudness' in fname:
+                prefixedfname = f'ref_{CONFIG["refSPL"]}_{fname}'
+                files = [ x for x in eq_files if prefixedfname in x]
+            else:
+                files = [ x for x in eq_files if fname in x]
+
+            if files:
+
+                if len (files) == 1:
+                    EQ_CURVES[ cnames[fname] ] = \
+                             np_loadtxt( f'{EQ_FOLDER}/{files[0]}' )
+                    pendings -= 1
+                else:
+                    print(f'(config) too much \'...{fname}\' '
+                           'files under share/eq/')
+            else:
+                print(f'(config) ERROR finding a \'...{fname}\' '
+                       'file under share/eq/')
+
+        #if not pendings:
+        if pendings == 0:
+            return EQ_CURVES
+        else:
+            return {}
+
+
+    try:
+        with open(f'{MAINFOLDER}/config/config.yml', 'r') as f:
+            CONFIG = yaml.safe_load(f)
+    except:
+        print(f'{Fmt.RED}(config) ERROR reading \'config.yml\'{Fmt.END}')
+        sys.exit()
+
+    LOUDSPEAKER         = CONFIG['loudspeaker']
+    LSPK_FOLDER         = f'{MAINFOLDER}/loudspeakers/{LOUDSPEAKER}'
+    BFCFG_PATH          = f'{LSPK_FOLDER}/brutefir_config'
+
+    EQ_CURVES = find_eq_curves()
+    if not EQ_CURVES:
+        print( '(config) ERROR loading EQ_CURVES from share/eq/' )
+        sys.exit()
+
+
+
+# AUTOEXEC
+_init()
