a4a65d72bf97aa1cc305c7ec1b663e1a2e5396a3
Rsantct <rsantct@gmail.com>
2022-01-11 11:39:32 +0100

    Update librespot.py

--- pe.audio.sys/share/scripts/librespot.py
+++ pe.audio.sys/share/scripts/librespot.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2019 Rafael Sánchez
-# This file is part of 'pe.audio.sys', a PC based personal audio system.
-
-# 'pe.audio.sys' is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# 'pe.audio.sys' is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with 'pe.audio.sys'.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright (c) Rafael Sánchez
+# This file is part of 'pe.audio.sys'
+# 'pe.audio.sys', a PC based personal audio system.
 
 """
     '/usr/bin/librespot': a headless Spotify Connect player daemon
