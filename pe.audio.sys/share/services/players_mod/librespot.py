6d2046ff97eb0513fd7611b4da8b6deeadca8d65
Rsantct <rsantct@gmail.com>
2022-10-08 10:09:13 +0200

    Update librespot.py

--- pe.audio.sys/share/services/players_mod/librespot.py
+++ pe.audio.sys/share/services/players_mod/librespot.py
@@ -5,6 +5,8 @@
 # 'pe.audio.sys', a PC based personal audio system.
 
 """ A libresport interface module for players.py
+    librespot is a Spotify Connect client:
+    https://github.com/librespot-org/librespot
 """
 import os
 import subprocess as sp
@@ -28,53 +30,61 @@ def librespot_control(cmd, arg=''):
         return 'play'
 
 
-# librespot (Spotify Connect client) metatata
 def librespot_meta(md):
     """ Input:  blank md dict
         Output: metadata dict derived from librespot printouts
         I/O:    .librespot_events (r) - librespot redirected printouts
     """
 
-    # Unfortunately librespot only prints out the title metadata, nor artist neither album.
-    # More info can be retrieved from the spotify web, but it is necessary to register
-    # for getting a privative and unique http request token for authentication.
+    # Unfortunately librespot only prints out the title metadata,
+    # nor artist neither album.
+    # More info can be retrieved from the spotify web, but it is needed
+    # to register for getting a privative and unique http request token
+    # for authentication.
 
     # Gets librespot bitrate from librespot running process:
     try:
-        tmp = sp.check_output( 'pgrep -fa /usr/bin/librespot'.split() ).decode()
-        # /usr/bin/librespot --name rpi3clac --bitrate 320 --backend alsa --device jack --disable-audio-cache --initial-volume=99
+        tmp = sp.check_output('pgrep -fa /usr/bin/librespot'.split()).decode()
+        # /usr/bin/librespot ... --bitrate 320 ...
         librespot_bitrate = tmp.split('--bitrate')[1].split()[0].strip()
     except:
         librespot_bitrate = '-'
 
+    # Minimum metadata
     md['player'] = 'librespot'
     md['bitrate'] = librespot_bitrate
 
+    # Trying to complete metadata fields:
     try:
+
         # Returns the current track title played by librespot.
-        # 'scripts/librespot.py' handles the libresport print outs to be
-        #                        redirected to 'tmp/.librespotEvents'
-        # example:
-        # INFO:librespot_playback::player: Track "Better Days" loaded
-        #
+        # 'scripts/librespot.py' redirects printouts to .librespot_events
+
+        # librespot uses to print out some 'AddrNotAvailable, message' mixed with
+        # playback info messages, so we will search for the latest
+        # 'Track ... loaded' message backwards from the end of the events file:
+
         with open(f'{MAINFOLDER}/.librespot_events', 'r') as f:
             lines = f.readlines()[-20:]
-        # Recently librespot uses to print out some 'AddrNotAvailable, message' mixed with
-        # playback info messages, so we will search for the latest 'Track ... loaded' message,
-        # backwards from the end of the events file:
+
         for line in lines[::-1]:
-            if line.strip()[-6:] == "loaded":
-                # raspotify flawors of librespot
-                if 'player] <' not in line:
+
+            if line.endswith("loaded"):
+
+                # Rust cargo format:
+                if 'player] <' in line:
+                    md['title'] = line.split('player] <')[-1] \
+                                      .split('> (')[0]
+
+                # former loaded message format:
+                else:
+                    title =
                     md['title'] = line.split('player: Track "')[-1] \
                                       .split('" loaded')[0]
-                    break
-                # Rust cargo librespot package
-                else:
-                    md['title'] = line.split('player] <')[-1] \
-                                      .split('> loaded')[0]
-                    break
+                break
+
     except:
         pass
+
 
     return md
