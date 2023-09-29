453a75b5b82e0f722e9224db9d31f0db27c1804e
Rsantct <rsantct@gmail.com>
2023-09-29 20:12:15 +0200

    Update power_amp_control.py

--- pe.audio.sys/share/plugins/power_amp_control.py
+++ pe.audio.sys/share/plugins/power_amp_control.py
@@ -13,7 +13,8 @@
 
 """
 
-from    subprocess import Popen
+from    subprocess import call, Popen
+from    time import sleep
 import  os
 import  sys
 
@@ -25,10 +26,31 @@ from    config import CONFIG
 
 if 'amp_manager' in CONFIG and CONFIG["amp_manager"]:
     amp_manager = CONFIG["amp_manager"]
-
 else:
     print(f"{__file__} ERROR reading 'amp_manager' inside 'config.yml'")
     sys.exit()
+
+
+def start():
+
+    call(f'{amp_manager} on'.split())
+
+    # wait for ~/.amplifier == on
+    tries = 20
+    while tries:
+        with open(f'{UHOME}/.amplifier', 'r') as f:
+            tmp = f.read().strip()
+            if 'on' in tmp or '1' in tmp:
+                print(f'(power_amp_control.py) detected ~/.amplifier = ON')
+                sleep(1)
+                break
+            sleep(.25)
+    if not tries:
+        print(f'(power_amp_control.py) ERROR detecting ~/.amplifier = ON')
+
+
+def stop():
+    Popen(f'{amp_manager} off'.split())
 
 
 if __name__ == '__main__':
@@ -36,10 +58,10 @@ if __name__ == '__main__':
     if sys.argv[1:]:
 
         if sys.argv[1] == 'start':
-            Popen(f'{amp_manager} on'.split())
+            start()
 
         elif sys.argv[1] == 'stop':
-            Popen(f'{amp_manager} off'.split())
+            stop()
 
         else:
             print(__doc__)
