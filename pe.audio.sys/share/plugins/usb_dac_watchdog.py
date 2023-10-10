89e3035e92b5ed28669dc88ba4bd2e5ead753137
Rsantct <rsantct@gmail.com>
2023-10-10 17:42:36 +0200

    Update usb_dac_watchdog.py

--- pe.audio.sys/share/plugins/usb_dac_watchdog.py
+++ pe.audio.sys/share/plugins/usb_dac_watchdog.py
@@ -13,96 +13,77 @@
 
     A false jackd process may occur after the USB DAC was disconnected.
 
+    Then, pe.audio.sys will be stopped.
+
     When the user does switch-on the USB DAC, then pe.audio.sys will restart.
 
-    Usage:      usb_dac_watchdog.py  start|stop
+    Usage:      usb_dac_watchdog.py  start
+
+    (i) This plugin is NOT intended to be configured under the
+        plugins section inside config.yml, PLEASE use a dedicated entry:
+
+            usb_dac_watchdog = true
 
 """
 import  subprocess as sp
 import  os
 import  sys
-from    time import sleep
+from    time     import sleep
+from    datetime import datetime
 
 UHOME = os.path.expanduser("~")
 sys.path.append(f'{UHOME}/pe.audio.sys/share/miscel')
 
-from config import CONFIG
+from miscel import  detect_USB_DAC, jackd_process, jackd_response, \
+                    CONFIG, LOG_FOLDER
 
-
-def detect_USB_DAC(cname):
-    """ Check if the provided card name is available,
-        and if it is USB type.
-    """
-    result = False
-    tmp = sp.check_output('aplay -l'.split()).decode().strip().split('\n')
-    for line in tmp:
-        if cname in line and 'USB' in line.upper():
-            result = True
-    return result
-
-
-def jackd_runs(cname=''):
-    """ Check the jackd process
-
-        (!) A false jackd process may occur after the USB DAC
-            was disconnected
-
-    """
-    def check_jack_lsp():
-        try:
-            sp.check_output('jack_lsp')
-            return True
-        except:
-            return False
-
-    result = False
-
-    try:
-        tmp = sp.check_output('pgrep -fla jackd'.split()).decode().strip()
-    except:
-        tmp = ''
-
-    if cname in tmp:
-        if check_jack_lsp():
-            result = True
-        else:
-            print('(usb_dac_watchdog.py) WARNING jackd process NOT RESPONDING')
-
-    return result
-
+CNAME   = CONFIG["jack"]["device"].split(':')[-1].split(',')[0]
+LOGPATH = f'{LOG_FOLDER}/usb_dac_watchdog.log'
 
 def peaudiosys_restart():
-    """ Restarts pe.audio.sys
-        (The script peaudiosys_restart.sh will waits for it to be running)
-    """
-    print('(usb_dac_watchdog.py) RESTARTING pe.audio.sys ... .. .')
-    sp.call(f'{UHOME}/bin/peaudiosys_restart.sh', shell=True)
+
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    with open(LOGPATH, 'w') as flog:
+        flog.write(f'{now} (usb_dac_watchdog.py) RESTARTING pe.audio.sys ... .. .\n')
+        flog.flush()
+        sp.call(f'{UHOME}/bin/peaudiosys_restart.sh', shell=True,
+                    stdout=flog, stderr=flog)
+
+
+def peaudiosys_stop():
+
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    with open(LOGPATH, 'w') as flog:
+        flog.write(f'{now} (usb_dac_watchdog.py) USB DAC \'{CNAME}\' NOT detected, stopping pe.audio.sys\n')
+        flog.write(f'{now} (usb_dac_watchdog.py) STOPPING pe.audio.sys ... .. .\n')
+        flog.flush()
+        sp.call(f'{UHOME}/bin/peaudiosys_restart.sh stop', shell=True,
+                    stdout=flog, stderr=flog)
 
 
 def start():
     """ MAIN LOOP
     """
-    card_name     = CONFIG["jack"]["device"].split(':')[-1].split(',')[0]
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+    with open(LOGPATH, 'w') as flog:
+        flog.write(f'{now} (usb_dac_watchdog.py) WATCHDOG STARTED.\n')
+
 
     while True:
 
-        if detect_USB_DAC(card_name):
+        if detect_USB_DAC(CNAME):
 
-            if jackd_runs(card_name):
-                print(f'(usb_dac_watchdog.py) JACK is running with the card \'{card_name}\'')
-
-            else:
+            if not jackd_response(CNAME):
                 peaudiosys_restart()
 
         else:
-            print(f'(usb_dac_watchdog.py) USB DAC \'{card_name}\' NOT detected')
 
-        sleep(3)
+            if jackd_process(CNAME):
+                peaudiosys_stop()
 
-
-def stop():
-    cmd = 'pkill -f "usb_dac_watchdog.py start"'
-    sp.call(cmd, shell=True)
+        sleep(10)
 
 
 if __name__ == "__main__":
@@ -110,8 +91,6 @@ if __name__ == "__main__":
     if sys.argv[1:]:
         if sys.argv[1] == 'start':
             start()
-        elif sys.argv[1] == 'stop':
-            stop()
         else:
             print(__doc__)
     else:
