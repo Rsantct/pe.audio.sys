9d49a3014ce8af198d968981829fcafd260dcd8f
Rsantct <rsantct@gmail.com>
2024-03-05 19:47:28 +0100

    Update shairport-sync.py

--- pe.audio.sys/share/plugins/shairport-sync.py
+++ pe.audio.sys/share/plugins/shairport-sync.py
@@ -3,17 +3,18 @@
 # Copyright (c) Rafael Sánchez
 # This file is part of 'pe.audio.sys'
 # 'pe.audio.sys', a PC based personal audio system.
-
 """
     shairport-sync  Plays audio streamed from AirPlay sources.
 
     use:    shairport-sync.py   start | stop
 """
-
 import sys
 from subprocess import Popen, call
 from socket import gethostname
 
+# Debian package system service NEEDS to be disabled after installing:
+#     sudo systemctl stop shairport-sync.service
+#     sudo systemctl disable shairport-sync.service
 
 def start():
     # Former versions used alsa but recent debian package alows jack :-)
