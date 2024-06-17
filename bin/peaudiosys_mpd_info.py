2e816d5ae846d7f1c97c8ed3da4b6aa6e089bdaf
Rsantct <rsantct@gmail.com>
2024-06-17 16:52:17 +0200

    Update peaudiosys_mpd_info.py

--- bin/peaudiosys_mpd_info.py
+++ bin/peaudiosys_mpd_info.py
@@ -5,9 +5,12 @@
 # 'pe.audio.sys', a PC based personal audio system.
 
 """ A command line MPD audio info tool
+
+    Usage:  peaudiosys_mpd_info.py [ -audio | -status | -current ]
 """
+import sys
 import mpd
-from time import sleep
+from   time import sleep
 
 
 def timesec2string(x):
@@ -49,19 +52,56 @@ def get_currentsong():
     return d
 
 
+def print_it(what):
+
+    if what == 'audio':
+
+        st = get_status()
+
+        for k in 'format', 'bitrate':
+            if k in st:
+                print(f'{ k.ljust(15) }: { st[k] }')
+            else:
+                print(f'{ k.ljust(15) }: --')
+
+    elif what == 'status':
+
+        for k, v in get_status().items():
+            print(f'{k.ljust(15)}: {v}')
+
+    elif what == 'current_song':
+
+        cs = get_currentsong()
+
+        for k in 'file', 'artist', 'album', 'title':
+            if k in cs:
+                print(f'{ k.ljust(15) }: { cs[k] }')
+            else:
+                print(f'{ k.ljust(15) }: --')
+
+
 if __name__ == "__main__":
 
     c = mpd.MPDClient()
     c.connect('localhost', 6600)
 
-    for k, v in get_status().items():
+    if not sys.argv[1:]:
 
-        print(f'{k.ljust(15)}: {v}')
+        print_it('status')
+        print()
+        print_it('current_song')
 
-    #print()
+    else:
+        if '-a' in sys.argv[1]:
+            print_it('audio')
 
-    #for k, v in get_currentsong().items():
-    #    print(f'{k.ljust(15)}: {v}')
+        elif '-s' in sys.argv[1]:
+            print_it('status')
 
+        elif '-c' in sys.argv[1]:
+            print_it('current_song')
+
+        else:
+            print(__doc__)
 
 
