03eb5e57a67b081d5f7d41b0db03af200de0e72e
Rsantct <rsantct@gmail.com>
2024-06-29 19:48:30 +0200

    Update librespot.py

--- pe.audio.sys/share/plugins/librespot.py
+++ pe.audio.sys/share/plugins/librespot.py
@@ -25,6 +25,8 @@ BINARY = '/usr/bin/librespot'
 
 # BACKEND OPTIONS
 BACKEND_OPTS = f'--backend jackaudio --device librespot'
+# If needed to use Pulseaudio in order to reesample to reach Jack+Brutefir @ 48 KHz
+#BACKEND_OPTS = f'--backend pulseaudio'
 
 # MORE OPTIONS LIST (do not configure here: bitrate, name, backend, device)
 MOREOPT = [
