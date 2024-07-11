cd4055867dc759401f1ca7a9417769d297536e19
Rsantct <rsantct@gmail.com>
2024-07-11 18:00:58 +0200

    Update main.js

--- pe.audio.sys/share/www/js/main.js
+++ pe.audio.sys/share/www/js/main.js
@@ -584,12 +584,14 @@ function page_update() {
 
 
     function aux_info_refresh(){
+
         if ( aux_info.amp == 'off' || aux_info.amp == 'on' ) {
             document.getElementById("bt_onoff").innerText = aux_info.amp.toUpperCase();
             document.getElementById("bt_onoff").style.display = 'block';
         }else{
             document.getElementById("bt_onoff").style.display = 'none';
         }
+
         if ( ! aux_info.last_macro ){
             clear_macro_buttons_highlight();
         }else{
@@ -598,6 +600,22 @@ function page_update() {
             clear_macro_buttons_highlight();
             highlight_macro_button(mName)
         }
+
+        let sysmon = '';
+        const temp = aux_info.sysmon.temp
+        const wifi = aux_info.sysmon.wifi
+
+        if (temp){
+            sysmon += 'temp: ' + temp + 'º';
+        }
+
+        if (! isEmpty(wifi)){
+            sysmon += ' | wifi: ' + wifi['Bit-rate-Mb/s'] + ' Mb/s';
+            sysmon += ' quality: ' + wifi['Quality'];
+            sysmon += ' Rx: ' + wifi['Signal-level'] + ' dBm';
+        }
+
+        document.getElementById("sysmon").innerText = sysmon;
     }
 
 
@@ -1116,6 +1134,7 @@ function ck_display_advanced(mode) {
         document.getElementById("format_file").style.display = "table-row";
         document.getElementById("div_advanced_controls").style.display = "block";
         document.getElementById("main_lside").style.display = "table-cell";
+        document.getElementById("sysmon").style.display = "table-cell";
         document.getElementById("SoloInfo").style.display = "table-cell";
         document.getElementById("PolarityInfo").style.display = "table-cell";
         document.getElementById("bt_aod").style.display = "inline-block";
@@ -1127,6 +1146,7 @@ function ck_display_advanced(mode) {
         document.getElementById("format_file").style.display = "none";
         document.getElementById("div_advanced_controls").style.display = "none";
         document.getElementById("main_lside").style.display = "none";
+        document.getElementById("sysmon").style.display = "none";
         document.getElementById("SoloInfo").style.display = "none";
         document.getElementById("PolarityInfo").style.display = "none";
         if ( state.extra_delay === 0 ) {
@@ -1279,6 +1299,11 @@ function allAreTrue(arr) {
   return arr.every(element => element === true);
 }
 
+
+function isEmpty(obj) {
+    // test if some object (say a dict) is empty
+    return Object.keys(obj).length === 0;
+}
 
 //////// ELEMENTS HIGHLIGHT ////////
 
