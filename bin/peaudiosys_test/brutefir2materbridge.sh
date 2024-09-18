3c060cb3f19d93655706620d484cf6b795ef7c3d
Rsantct <rsantct@gmail.com>
2024-09-18 22:33:58 +0200

    Update brutefir2materbridge.sh

--- bin/peaudiosys_test/brutefir2materbridge.sh
+++ bin/peaudiosys_test/brutefir2materbridge.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+
+if [[ $1 == *"-k"* ]]; then
+    pkill meterbridge
+    exit 0
+
+elif [[ $1 == *"-h"* ]]; then
+
+    echo "Usage:   brutefir2meterbridge.sh  [--kill]"
+    exit 0
+
+fi
+
+
 pkill meterbridge
+
 
 meterbridge -c 3 -t dpm -n Meter_L \
     brutefir:lo.L \
