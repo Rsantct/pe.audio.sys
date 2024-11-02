a1c6e6075b177ed5776bf5af968cf3dc57b0663e
Rsantct <rsantct@gmail.com>
2024-11-03 00:02:32 +0100

    Update cdda.py

--- pe.audio.sys/share/services/players_mod/cdda.py
+++ pe.audio.sys/share/services/players_mod/cdda.py
@@ -28,8 +28,8 @@ from    os.path import expanduser
 UHOME = expanduser("~")
 sys.path.append(f'{UHOME}/pe.audio.sys/share/miscel')
 
-from    config import CONFIG, CDDA_INFO_PATH
-from    fmt    import Fmt
+from    miscel import CONFIG, CDDA_INFO_PATH, CDDA_INFO_TEMPLATE, \
+                      Fmt, msec2str, read_mpd_config
 
 try:
     import  discid
@@ -46,43 +46,38 @@ else:
     CDROM_DEVICE = '/dev/cdrom'
     print(f'{Fmt.BLUE}(cdda.py) Using default \'{CDROM_DEVICE}\'{Fmt.END}')
 
-# cd port under Jack
-try:
-    CD_JACK_PNAME = CONFIG['sources']['cd']['jack_pname']
-except:
-    CD_JACK_PNAME = 'mplayer_cdda'
 
 # cdda info template with a fake track #1
-CDDA_INFO_TEMPLATE = { 'discid':'', 'artist': '-', 'album': '-',
-                       '1':      {'title': '-', 'length': '00:00.00'}
+CDDA_INFO_TEMPLATE = { 'discid': '', 'artist': '-', 'album': '-',
+                       'tracks': {
+                                    '1': {'title': '-', 'length': '00:00.00'}
+                       }
                      }
 
-
-def mmsscc2msec(mmsscc):
-    """ input:   'mm:ss.cc' (str)
-        output:  millisecs  (int)
-    """
-    mm   = int( mmsscc.split(':')[0] )
-    sscc =      mmsscc.split(':')[1]
-    ss   = int( sscc.split('.')[0]   )
-    cc   = int( sscc.split('.')[1]   )
-
-    millisec = mm * 60 * 1000 + ss * 1000 + cc * 10
-
-    return millisec
+CDDA_MUSICBRAINZ_PATH = f'{UHOME}/pe.audio.sys/.cdda_musicbrainz'
 
 
-def msec2string(msec):
-    """ input:  millisecs  (float)
-        output: 'mm:ss.cc' (str)
-    """
-    sec  = msec / 1e3
-    mm   = f'{sec // 60:.0f}'.zfill(2)
-    ss   = f'{sec %  60:.2f}'.zfill(5)
-    return f'{mm}:{ss}'
+def _get_disc_metadata(device=CDROM_DEVICE):
 
+    def find_medium_list_idx(disc_id, medium_list_items):
+        """
+            Double CD sets, will have several medium-list items
+            and we need to choose the one matching our disc.id
+        """
 
-def read_disc_metadata(device=CDROM_DEVICE):
+        idx = 0
+
+        for i, item in enumerate(medium_list_items):
+
+            for dl_item in item["disc-list"]:
+
+                if disc_id==dl_item["id"]:
+
+                    idx = i
+                    break
+
+        return idx
+
 
     def simple_md(disc):
         """ For disc not found, we can derive the tracks and tracks length
@@ -109,7 +104,7 @@ def read_disc_metadata(device=CDROM_DEVICE):
             trackNum = i
             trackLen = ( track_sectors[i] - track_sectors[i - 1] ) / 75
             md[str(trackNum)] = {'title': 'track ' + f'{trackNum}'.zfill(2),
-                                 'length': msec2string(trackLen * 1e3)}
+                                 'length': msec2str(trackLen * 1e3)}
 
         return md
 
@@ -124,12 +119,16 @@ def read_disc_metadata(device=CDROM_DEVICE):
         global CDDA_DISCID
         CDDA_DISCID  = disc.id
     except:
-        print('(cdaa.py) not CDDA found')
+        print(f'{Fmt.RED}(cdaa.py) not CDDA found{Fmt.END}')
         return md
 
     try:
         result = mz.get_releases_by_discid( disc.id,
                                       includes=['artists', 'recordings'] )
+        with open(CDDA_MUSICBRAINZ_PATH, 'w') as f:
+            f.write( json.dumps( result ) )
+        print(f'{Fmt.BLUE}(cdda.py) Full musicbrainz CD data saved to {CDDA_MUSICBRAINZ_PATH}{Fmt.END}')
+
     except Exception as e:
         print(f'{Fmt.BOLD}(cdda.py) disc not found or bad response: {str(e)}{Fmt.END}')
         return simple_md(disc)
@@ -137,7 +136,7 @@ def read_disc_metadata(device=CDROM_DEVICE):
     # most of discs are 'disc' musicbrainz kinf of
     if result.get('disc'):
 
-        print(f'(cdda.py) musicbrainz got \'disc\': {disc.id}' )
+        print(f'{Fmt.BLUE}(cdda.py) musicbrainz got \'disc\': {disc.id}{Fmt.END}' )
 
         mz_md = result['disc']['release-list'][0]
 
@@ -145,7 +144,10 @@ def read_disc_metadata(device=CDROM_DEVICE):
 
         md['album']  = mz_md['title']
 
-        track_list   = mz_md['medium-list'][0]['track-list']
+        # (!) Double CD sets, will have several medium-list
+        medium_list_idx = find_medium_list_idx( disc.id, mz_md['medium-list'] )
+
+        track_list   = mz_md['medium-list'][medium_list_idx]['track-list']
 
     # but somo are 'cdstub' musicbrainz kind of
     elif result.get('cdstub'):
@@ -172,103 +174,127 @@ def read_disc_metadata(device=CDROM_DEVICE):
     for pos, track in enumerate( track_list ):
 
         # Retrieve track length
+
         # from normal 'disc':
         if 'recording' in track and 'length' in track['recording']:
-            lengthStr = msec2string( float(track['recording']['length']) )
+            lengthStr = msec2str( float(track['recording']['length']) )
+
         # from some 'cdstub':
         elif 'length' in track:
-            lengthStr = msec2string( float(track['length']) )
+            lengthStr = msec2str( float(track['length']) )
+
         # from some 'cdstub':
         elif 'track_or_recording_length' in track:
-            lengthStr = msec2string( float(track['track_or_recording_length']) )
+            lengthStr = msec2str( float(track['track_or_recording_length']) )
         else:
-            lengthStr = msec2string( 0.0 )
+            lengthStr = msec2str( 0.0 )
 
         # Retrieve track title
         if 'recording' in track and 'title' in track['recording']:
             track_title = track['recording']['title']
+
         elif 'title' in track:
             track_title = track['title']
 
+        # Some track names can contain the album title, this is redundant
+        if md["album"] in track_title:
+            track_title = track_title.replace(md["album"], '')
+            if track_title[0] in (':', ',', '.'):
+                track_title = track_title[1:].strip()
+
         # adding to our metadata disc structure
-        md[ str(pos + 1) ] = { 'title':  track_title,
+        md["tracks"][ str(pos + 1) ] = { 'title':  track_title,
                                'length': lengthStr }
 
     return md
 
 
-def save_disc_metadata(device=CDROM_DEVICE):
-    with open(CDDA_INFO_PATH, 'w') as f:
-        f.write( json.dumps( read_disc_metadata(device) ) )
-
-
-def make_pls():
-
-    md = read_disc_metadata()
-
-    pls =   '<?xml version="1.0" encoding="UTF-8"?>\n'
-    pls +=  '<playlist version="1" xmlns="http://xspf.org/ns/0/">\n'
-    pls +=  '  <trackList>\n'
-
-    for k in md.keys():
-
-        if not k.isdigit():
-            continue
-
-        duration = mmsscc2msec( md[k]["length"] )
-
-        pls +=  '    <track>\n'
-        pls += f'      <location>cdda:///{k}</location>\n'
-        pls += f'      <creator>{md["artist"]}</creator>\n'
-        pls += f'      <album>{md["album"]}</album>\n'
-        pls += f'      <title>{md[k]["title"]}</title>\n'
-        pls += f'      <duration>{duration}</duration>\n'
-        pls +=  '    </track>\n'
-
-    pls +=  '  </trackList>\n'
-    pls +=  '</playlist>\n'
-
-    return pls
-
-
-def make_m3u():
-
-    md = read_disc_metadata()
-
-    m3u =   '#EXTM3U\n'
-
-    for k in md.keys():
-
-        if not k.isdigit():
-            continue
-
-        durationms = mmsscc2msec( md[k]["length"] )
-        durationsec = str( int(round( durationms / 1e3, 0)) )
-
-        m3u += '#EXTINF:'
-        m3u += f'{durationsec},'
-        m3u += f'{md[k]["title"]}\n'
-        m3u += f'cdda:/{CDROM_DEVICE}/{k}\n'
-
-    return m3u
-
-
-def save_cdda_playlist(mode='m3u'):
+def _save_cdda_playlist(md={}, mode='m3u'):
     """ Saves a playlist for MPD :-)
+        (PLS mode is not tested)
     """
-    folder = f'{UHOME}/.config/mpd/playlists'
+
+    def make_m3u(md):
+
+        m3u =   '#EXTM3U\n'
+
+        for k, v in md["tracks"].items():
+
+            durationms = msec2str( string=v["length"] )
+            durationsec = str( int(round( durationms / 1e3, 0)) )
+
+            m3u += '#EXTINF:'
+            m3u += f'{durationsec},'
+            m3u += f'{v["title"]}\n'
+            m3u += f'cdda:/{CDROM_DEVICE}/{k}\n'
+
+        return m3u
+
+
+    def make_pls(md):
+
+        pls =   '<?xml version="1.0" encoding="UTF-8"?>\n'
+        pls +=  '<playlist version="1" xmlns="http://xspf.org/ns/0/">\n'
+        pls +=  '  <trackList>\n'
+
+        for k, v in md["tracks"].items():
+
+            duration = msec2str( string=v["length"] )
+
+            pls +=  '    <track>\n'
+            pls += f'      <location>cdda:///{k}</location>\n'
+            pls += f'      <creator>{md["artist"]}</creator>\n'
+            pls += f'      <album>{md["album"]}</album>\n'
+            pls += f'      <title>{v["title"]}</title>\n'
+            pls += f'      <duration>{duration}</duration>\n'
+            pls +=  '    </track>\n'
+
+        pls +=  '  </trackList>\n'
+        pls +=  '</playlist>\n'
+
+        return pls
+
+
     if mode == 'm3u':
-        tmp = make_m3u()
-        fname = f'{folder}/.cdda.m3u'
+        tmp = make_m3u( md )
+        fname = f'{ read_mpd_config()["playlist_directory"] }/cdda.m3u'
+
     elif mode == 'pls':
-        tmp = make_pls()
-        fname = f'{folder}/.cdda.pls'
+        tmp = make_pls( md )
+        fname = f'{ read_mpd_config()["playlist_directory"] }/cdda.pls'
+
     else:
         return
+
     with open(f'{fname}', 'w') as f:
         f.write(tmp)
 
+    print(f'{Fmt.BLUE}(cdda.py) MPD CD playlist saved to {fname}{Fmt.END}')
+
+
+
+def dump_cdda_metadata(device=CDROM_DEVICE):
+    """ dump CD-Audio disc matadata to:
+
+            pe.audio.sys/.cdda_info
+
+            {MPD_playlists_folder}/cdda_mpd_playlist.m3u
+    """
+
+    md = _get_disc_metadata(device)
+
+    with open(CDDA_INFO_PATH, 'w') as f:
+        f.write( json.dumps( md ) )
+
+    if md["discid"]:
+
+        print(f'{Fmt.BLUE}(cdda.py) CD metadata saved to {CDDA_INFO_PATH}{Fmt.END}')
+
+        _save_cdda_playlist( md )
+
+
 
 if __name__ == "__main__":
-    if sys.argv[1:] and '-s' in sys.argv[1]:
-        save_cdda_playlist()
+
+    if sys.argv[1:] and '-d' in sys.argv[1]:
+        dump_cdda_metadata()
