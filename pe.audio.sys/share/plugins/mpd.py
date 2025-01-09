eec6bd8f7ddc79f3acf135d3d3942384bcef5731
Rsantct <rsantct@gmail.com>
2025-01-09 21:02:15 +0100

    Update mpd.py

--- pe.audio.sys/share/plugins/mpd.py
+++ pe.audio.sys/share/plugins/mpd.py
@@ -23,6 +23,51 @@ from time import sleep
 from getpass import getuser
 
 UHOME = os.path.expanduser("~")
+sys.path.append(f'{UHOME}/pe.audio.sys/share/miscel')
+
+from miscel import read_mpd_config
+
+
+def get_mpd_config_path():
+    """ If the default MPD config file wants to use a mounted filesystem,
+        this checks for it to be available, and if necessary will generate
+        an alternative file .mpdconf.local
+
+        returns: the .mpdconf path to run MPD with.
+    """
+
+    def do_mpd_conf_local():
+        """ Makes a copy of .mpdconf but using a local `playlist_directory`
+        """
+
+        new_mpd_conf_path = MPD_CONF_PATH + '.local'
+
+        new_local_pl_dir = f'{UHOME}/.config/mpd/playlists'
+
+        with open(MPD_CONF_PATH, 'r') as f:
+            mpdconfiglines = f.read().split('\n')
+
+        for i, line in enumerate(mpdconfiglines):
+            if 'playlist_directory' in line and line.strip()[0] != '#':
+                mpdconfiglines[i] = f'playlist_directory      "{new_local_pl_dir}"'
+
+        os.makedirs(new_local_pl_dir, exist_ok=True)
+
+        with open(new_mpd_conf_path, 'w') as f:
+            for line in mpdconfiglines:
+                f.write( line + '\n' )
+
+
+    MPD_CONF_PATH = f'{UHOME}/.mpdconf'
+
+    MPD_PL_DIR  = read_mpd_config(MPD_CONF_PATH)["playlist_directory"]
+
+    if not os.path.isdir( MPD_PL_DIR ):
+
+        do_mpd_conf_local()
+        MPD_CONF_PATH += '.local'
+
+    return MPD_CONF_PATH
 
 
 def check_systemd_service():
@@ -50,9 +95,13 @@ def stop():
 
 
 def start():
+
     check_systemd_service()
+
+    MPD_CONF_PATH = get_mpd_config_path()
+
     with open('/dev/null', 'w') as fnull:
-        Popen( f'mpd {UHOME}/.mpdconf'.split(), stdout=fnull, stderr=fnull )
+        Popen( f'mpd {MPD_CONF_PATH}'.split(), stdout=fnull, stderr=fnull )
 
 
 if __name__ == '__main__':
