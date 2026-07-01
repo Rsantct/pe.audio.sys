0c3e2857cdadb1d173245119ec8b9bc5616c7909
Rsantct <rsantct@gmail.com>
2026-07-01 18:40:29 +0200

    Implement TCP server in miscel.py
    Added a general purpose TCP server function with client handling.

--- pe.audio.sys/share/miscel/miscel.py
+++ pe.audio.sys/share/miscel/miscel.py
@@ -873,6 +873,45 @@ def wait4ports( pattern, timeout=10 ):
         return False
 
 
+def tcp_server(addr='127.0.0.1', port=0, service_id='UNNAMED', processor=None, verbose=False):
+    """ a general purpose TCP server
+    """
+
+    def handle_client(srv):
+
+        con, cliaddr = srv.accept()
+
+        with con:
+
+            msg = con.recv(1024).decode().strip()
+            if verbose:
+                print( f'(server-{service_id}) Rx: {msg}' )
+
+            result = ''
+            if processor:
+
+                result = processor( addr=cliaddr[0], msg=msg )
+
+                if result:
+                    # Sending back the result
+                    con.sendall( result.encode() )
+                    if verbose:
+                        print( f'(server-{service_id}) Tx: {result}' )
+
+
+    if not port:
+        raise Exception('(miscel.server) missing port')
+
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv.bind((addr, port))
+    srv.listen(10)
+
+    while True:
+        handle_client(srv)
+
+
+
 def send_cmd( cmd, sender='', verbose=False, timeout=60,
               host='127.0.0.1', port=CONFIG['peaudiosys_port'] ):
     """
