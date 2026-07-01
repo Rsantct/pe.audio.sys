139aebc88b61a125e59d80b892d44acd886d33e7
Rsantct <rsantct@gmail.com>
2026-07-01 21:17:55 +0200

    Document remote_send_cmd function
    Add a docstring to the remote_send_cmd function.

--- pe.audio.sys/share/plugins/remote_volume_daemon.py
+++ pe.audio.sys/share/plugins/remote_volume_daemon.py
@@ -185,6 +185,8 @@ def discover_remotes():
 
 
 def remote_send_cmd(cli_addr, cmd):
+    """ this is threaded
+    """
 
     print( f'(remote_volume_daemon) remote {cli_addr} sending \'{cmd}\'' )
 
