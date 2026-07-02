42926923a9d7ac5544a723850d34164cc8356d08
Rsantct <rsantct@gmail.com>
2026-07-02 13:00:03 +0200

    Update print statements to include 'daemon' in messages

--- pe.audio.sys/share/plugins/remote_volume_daemon.py
+++ pe.audio.sys/share/plugins/remote_volume_daemon.py
@@ -260,7 +260,7 @@ def listen_to_preamp():
             REMOTE_CLIENTS.pop( addr, None )
 
 
-    print( f'(remote_volume) Keep relaying level changes to remotes ...' )
+    print( f'(remote_volume_daemon) Keep relaying level changes to remotes ...' )
 
     # Start a server listening to LOCAL
     job = threading.Thread(
@@ -295,7 +295,7 @@ def listen_to_remotes():
 
             if cli_addr != my_ip and '127.0.' not in cli_addr:
 
-                print( f'(remote_volume) Received hello from: {cli_addr}' )
+                print( f'(remote_volume_daemon) Received hello from: {cli_addr}' )
 
                 if cli_addr not in REMOTE_CLIENTS:
 
@@ -319,7 +319,7 @@ def listen_to_remotes():
 
 
 
-    print( f'(remote_volume) Keep listening for new remotes ...' )
+    print( f'(remote_volume_daemon) Keep listening for new remotes ...' )
 
     # Start a server listening to ALL
     job = threading.Thread(
@@ -355,7 +355,7 @@ if __name__ == "__main__":
     my_hostname     = socket.gethostname()
     my_ip           = socket.gethostbyname(f'{my_hostname}.local')
     if not my_ip:
-        print( f'(remote_volume) ERROR GETTING MY IP ADDRESS !!!')
+        print( f'(remote_volume_daemon) ERROR GETTING MY IP ADDRESS !!!')
         exit()
 
 
